a0272b17fd83ac0a87dacb7885eb4c5b41a6571a
krivine <krivine@Rostand.(none)>
2011-07-18 09:36:06 +0200

    dot and plus work in progress

---
+++ simulation/nonLocal.ml
@@ -0,0 +1,9 @@
+(**Module that manages connectedness of injection images --for dot and plus operators*)
+open Mods
+
+type constraints = 
+
+(*returns a tripe corresponding to the coordinates of an injection whose image *)
+(* is probably connected to the image of the injection whose coordinates are given as argument*)
+(*let prob_connected (mix_id,cc_id,inj_id) state = *)
+	
