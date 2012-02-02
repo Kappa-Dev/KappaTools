a5703aafb5f896c0cce272e80ca316969d662de5
feret <feret@origan.(none)>
2012-02-02 22:45:16 +0100

    Linking Cflow with new KaSim version.

--- simulation/run.ml
+++ simulation/run.ml
@@ -3,7 +3,7 @@ open Tools
 open ExceptionDefn
 open Random_tree
 
-let event state grid counter plot env =
+let event state grid event_list counter plot env =
 	(*1. Time advance*)
 	let dt = 
 		let rd = Random.float 1.0 
@@ -72,7 +72,7 @@ let event state grid counter plot env =
 	(*4. Positive update*)
 	Counter.inc_events counter ;
 	
-	let env,state,pert_ids',grid = 
+	let env,state,pert_ids',grid,event_list = 
 		match opt_new_state with
 			| Some ((env,state,side_effect,embedding_t,psi,pert_ids),r) ->
 				
@@ -88,19 +88,20 @@ let event state grid counter plot env =
 				in
 				
 				(****************END POSITIVE UPDATE*****************)
-				
-					let grid = 
+				let phi = State.map_of embedding_t in 
+					let grid,event_list = 
 						if !Parameter.causalModeOn then
-							Causal.record r.Dynamics.lhs (Some (r.Dynamics.pre_causal,side_effect,psi,false,r.Dynamics.r_id)) (State.map_of embedding_t) state counter false grid env
-						else grid
+							Causal.record r.Dynamics.lhs (Some (r.Dynamics.pre_causal,side_effect,psi,false,r.Dynamics.r_id)) (State.map_of embedding_t) state counter false grid env,
+Kappa_instantiation.Cflow_linker.store_event (Kappa_instantiation.Cflow_linker.import_event (r,phi,psi)) event_list 
+						else grid,event_list 
 					in
-					(env,state,IntSet.union pert_ids pert_ids',grid)
+					(env,state,IntSet.union pert_ids pert_ids',grid,event_list)
 			| None ->
 				begin
 					if !Parameter.debugModeOn then Debug.tag "Null (clash or doesn't satisfy constraints)"; 
 					Counter.inc_null_events counter ; 
 					Counter.inc_consecutive_null_events counter ;
-					(env,state,IntSet.empty,grid)
+					(env,state,IntSet.empty,grid,event_list)
 				end
 	in
 	
@@ -108,9 +109,9 @@ let event state grid counter plot env =
 	let state,env = External.try_perturbate state (IntSet.union pert_ids pert_ids') counter env 
 	in
 	(*Profiling.add_chrono "Pert" Parameter.profiling t_pert ;*) 
-	(state,grid,env)
+	(state,grid,event_list,env)
 					
-let rec loop state grid counter plot env =
+let rec loop state grid event_list counter plot env =
 	if !Parameter.debugModeOn then 
 		Debug.tag (Printf.sprintf "[**Event %d (Activity %f)**]" counter.Counter.events (Random_tree.total state.State.activity_tree));
 	if Counter.is_initial counter then
@@ -119,13 +120,19 @@ let rec loop state grid counter plot env =
 			Plot.output state counter.Counter.time counter.Counter.events plot env counter
 		end ;
 	if (Counter.check_time counter) && (Counter.check_events counter) then
-		let state,grid,env = event state grid counter plot env 
+		let state,grid,event_list,env = event state grid event_list counter plot env 
 		in
-		loop state grid counter plot env
+		loop state grid event_list counter plot env
 	else
 		begin
-			if !Parameter.causalModeOn then Causal.dump grid state env ;
-			Plot.fill state counter plot env 0.0; (*Plotting last measures*)
-			Plot.flush_ticks counter ;
-			Plot.close plot
+			if !Parameter.causalModeOn then 
+			  begin
+			    Causal.dump grid state env ;
+			    let refined_event_list = Compression_main.weak_compression env event_list 
+                            in ()	    
+			  end;
+
+		  Plot.fill state counter plot env 0.0; (*Plotting last measures*)
+		  Plot.flush_ticks counter ;
+		  Plot.close plot
 		end			
