42daa2e91fb6924f47d2f67dc3ab3421fd8d70fc
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2014-10-06 15:26:48 +0200

    An interpreter of variables

--- simulation/state.ml
+++ simulation/state.ml
@@ -196,6 +196,82 @@ let instances_of_square ?(disjoint=false) mix_id radius_def state env =
 			if is_connex  then cont else (embedding,codomain,inj_list)::cont
 		) [] embeddings 
 
+let value_state_alg_op state counter ?(time=Counter.time counter) env = function
+  | Term.CPUTIME -> Nbr.F (Sys.time ())
+  | Term.TIME_VAR -> Nbr.F time
+  | Term.EVENT_VAR -> Nbr.I (Counter.event counter+Counter.null_event counter)
+  | Term.NULL_EVENT_VAR -> Nbr.I (Counter.null_event counter)
+  | Term.PROD_EVENT_VAR ->Nbr.I (Counter.event counter)
+
+type alg_stack_element =
+  | TO_EXEC_ALG of Term.bin_alg_op * Expr.alg_expr
+  | TO_EXEC_COMP of Term.compare_op * Expr.alg_expr
+  | TO_EXEC_BOOL of Term.bool_op * Expr.alg_expr Ast.bool_expr
+  | TO_COMPUTE_ALG of Term.bin_alg_op * Nbr.t
+  | TO_COMPUTE_COMP of Term.compare_op * Nbr.t
+  | TO_COMPUTE_UN of Term.un_alg_op
+
+let rec exec_alg :
+type a. t -> Counter.t -> ?time:float -> Environment.t ->
+     (t -> Counter.t -> ?time:float -> Environment.t ->
+      Nbr.t -> alg_stack_element list -> a) ->
+     Expr.alg_expr -> alg_stack_element list -> a =
+    fun state counter ?time env with_value alg sk ->
+    match alg with
+    | Expr.BIN_ALG_OP (op,(a,_),(b,_)) ->
+       exec_alg state counter ?time env with_value a (TO_EXEC_ALG (op,b)::sk)
+    | Expr.UN_ALG_OP (op,(a,_)) ->
+       exec_alg state counter ?time env with_value a (TO_COMPUTE_UN op::sk)
+    | Expr.STATE_ALG_OP (op) ->
+       with_value state counter ?time env (value_state_alg_op state counter ?time env op) sk
+    | Expr.ALG_VAR i ->
+       exec_alg state counter ?time env with_value
+		(fst (snd (fst env.Environment.algs).(i))) sk
+    | Expr.KAPPA_INSTANCE i ->
+       with_value state counter ?time env (instance_number i state env) sk
+    | Expr.TOKEN_ID i ->
+       with_value state counter ?time env (Nbr.F (state.token_vector.(i))) sk
+    | Expr.CONST n ->
+       with_value state counter ?time env n sk
+
+let rec with_value_alg state counter ?time env n = function
+  | [] -> n
+  | TO_EXEC_ALG (op,alg) :: sk ->
+     exec_alg state counter ?time env with_value_alg alg (TO_COMPUTE_ALG (op,n)::sk)
+  | TO_COMPUTE_ALG (op,n1) :: sk ->
+     with_value_alg state counter ?time env (Nbr.of_bin_alg_op op n1 n) sk
+  | TO_COMPUTE_UN op :: sk ->
+     with_value_alg state counter ?time env (Nbr.of_un_alg_op op n) sk
+  | _ -> failwith "type error in with_value_alg"
+and with_value_alg_bool state counter ?time env n = function
+  | TO_EXEC_COMP (op,alg) :: sk ->
+     exec_alg state counter ?time env with_value_alg_bool alg (TO_COMPUTE_COMP (op,n)::sk)
+  | TO_COMPUTE_COMP (op,n1) :: sk ->
+     with_value_bool state counter ?time env (Nbr.of_compare_op op n1 n) sk
+  | _ -> failwith "type error in with_value_alg_bool"
+and exec_bool state counter ?time env expr sk =
+  match expr with
+  | Ast.TRUE -> with_value_bool state counter ?time env true sk
+  | Ast.FALSE -> with_value_bool state counter ?time env false sk
+  | Ast.BOOL_OP (op,(a,_),(b,_)) ->
+     exec_bool state counter ?time env a (TO_EXEC_BOOL (op,b) :: sk)
+  | Ast.COMPARE_OP (op,(a,_),(b,_)) ->
+     exec_alg state counter ?time env with_value_alg_bool a (TO_EXEC_COMP (op,b) :: sk)
+and with_value_bool state counter ?time env b = function
+  | [] -> b
+  | TO_EXEC_BOOL (Term.OR,expr) :: sk when b ->
+     with_value_bool state counter ?time env true sk
+  | TO_EXEC_BOOL (Term.AND,expr) :: sk when not b ->
+     with_value_bool state counter ?time env false sk
+  | TO_EXEC_BOOL (op,expr) :: sk ->
+     exec_bool state counter ?time env expr sk
+  | _ -> failwith "type error in with_value_bool"
+
+let value_bool state counter ?time env expr =
+  exec_bool state counter ?time env expr []
+let value_alg state counter ?time env alg =
+  exec_alg state counter ?time env with_value_alg alg []
+
 let value state counter ?(time=Counter.time counter) env =
   let rec aux : type a. a Primitives.variable -> a =
 		    function
