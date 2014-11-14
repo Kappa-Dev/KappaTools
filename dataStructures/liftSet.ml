f751a074607223dcc896aca86399bbea1cea7d7f
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2014-11-14 15:50:07 +0100

    factorization

--- dataStructures/liftSet.ml
+++ dataStructures/liftSet.ml
@@ -4,22 +4,24 @@ type t = {mutable injs : InjSet.t}
 
 let iter f ls = InjSet.iter f ls.injs
 
+let elements ls = InjSet.elements ls.injs
+
 let fold f ls cont = InjSet.fold f ls.injs cont
 
 let add ls inj = (ls.injs <- InjSet.add inj ls.injs);ls
-	
+
 let remove ls inj = ls.injs <- InjSet.remove inj ls.injs
 
 let create s = {injs = InjSet.empty}
 
-let flush ls = ls.injs <- InjSet.empty 
+let flush ls = ls.injs <- InjSet.empty
 
 let empty () = {injs = InjSet.empty}
 
 let mem inj ls = InjSet.mem inj ls.injs
 
 let exists pred ls =
-	try
-		fold (fun inj b -> if pred inj then raise ExceptionDefn.True else b) ls false
-	with 
-		| ExceptionDefn.True -> true
+  try
+    fold (fun inj b ->
+	  if pred inj then raise ExceptionDefn.True else b) ls false
+  with ExceptionDefn.True -> true
--- siteGraphs/node.ml
+++ siteGraphs/node.ml
@@ -4,134 +4,143 @@ open ExceptionDefn
 (**node: (name,array[site_name,internal_state,link])*)
 (**ptr(i,a) pointer to a node and a site at offset i*)
 
-type port_status = (int option * ptr) (*internal state,link state*)
-and port = {status:port_status ; mutable dep : (LiftSet.t * LiftSet.t)} (*dep : (lifts for int,lifts for lnk)*)
-and t = {name:int ; interface : port array ; mutable address : int option} 
-(*prob_connect: mix_id -> {u | v....u and v,u are the image of the roots of connected components of mix_id} *)
-and ptr = Null | Ptr of (t * int) | FPtr of (int * int)
-and node = t (*just alias for Node.t*)
+type ptr = Null | Ptr of (t * int) | FPtr of (int * int)
+and port_status = (int option * ptr) (*internal state,link state*)
+and port = {status:port_status; mutable dep : (LiftSet.t * LiftSet.t)}
+(*dep : (lifts for int,lifts for lnk)*)
+and t = {name:int ; interface : port array ; mutable address : int option}
+ (* prob_connect: mix_id -> {u | v....u and v,u are the image of the roots of
+    connected components of mix_id} *)
+
+type node = t (*just alias for Node.t*)
 
 let name node = node.name
 let interface node = node.interface
 let set_address node i = node.address <- Some i
-let get_address node = match node.address with None -> raise Not_found | Some addr -> addr
-let is_empty node = (node.name = -1)
+let get_address node =
+  match node.address with None -> raise Not_found | Some addr -> addr
+let is_empty node = node.name = -1
 
-let get_lifts u i = 
-	let port_i = try u.interface.(i) with Invalid_argument msg -> invalid_arg (Printf.sprintf "Node.get_lifts: node %d has no site %d" (name u) i)
-	in
-	port_i.dep 
-	
-	
-let fold_status f node cont = 
-	let cont = ref cont in
-		Array.iteri (fun site_id port -> cont := f site_id port.status !cont) (interface node) ;
-		!cont
+let get_lifts u i =
+  try u.interface.(i).dep
+  with Invalid_argument msg ->
+    invalid_arg (Format.sprintf
+		   "Node.get_lifts: node %d has no site %d" (name u) i)
+
+let fold_status f node cont =
+  let cont = ref cont in
+  Array.iteri
+    (fun site_id port -> cont := f site_id port.status !cont) (interface node);
+  !cont
 
 let fold_dep f node cont =
-	let cont = ref cont in
-		Array.iteri (fun site_id port -> cont := f site_id port.dep !cont) (interface node) ;
-		!cont
-		
-let fold_interface f node cont = 
-	let cont = ref cont in
-		Array.iteri (fun site_id port -> cont := f site_id port !cont) (interface node) ;
-		!cont
-	
-let label node env =
-	let str_intf = 
-		fold_interface
-		(fun i port cont ->
-				let s_i = Environment.site_of_id node.name i env in
-				if (s_i = "_") then cont
-				else
-					let (int_state,_) = port.status
-					in 
-					match int_state with 
-						| None -> cont 
-						| Some x -> 
-							let s_int = ("~"^(Environment.state_of_id node.name i x env))
-							in
-							(Printf.sprintf "%s%s" s_i s_int)::cont
-		) node [] 
-	in
-	match str_intf with
-		| [] -> Environment.name node.name env
-		| _ -> Printf.sprintf "%s(%s)" (Environment.name node.name env) (String.concat "," (List.rev str_intf)) 
+  let cont = ref cont in
+  Array.iteri
+    (fun site_id port -> cont := f site_id port.dep !cont) (interface node);
+  !cont
 
-	
+let fold_interface f node cont =
+  let cont = ref cont in
+  Array.iteri
+    (fun site_id port -> cont := f site_id port !cont) (interface node);
+  !cont
+
+let label node env =
+  let str_intf =
+    fold_interface
+      (fun i port cont ->
+       let s_i = Environment.site_of_id node.name i env in
+       if (s_i = "_") then cont
+       else
+	 let (int_state,_) = port.status in
+	 match int_state with
+	 | None -> cont
+	 | Some x ->
+	    let s_int = ("~"^(Environment.state_of_id node.name i x env))
+	    in
+	    (Printf.sprintf "%s%s" s_i s_int)::cont
+      ) node []
+  in
+  match str_intf with
+  | [] -> Environment.name node.name env
+  | _ ->
+     Format.asprintf "%s(%a)" (Environment.name node.name env)
+		     (Pp.list Pp.space Pp.string) (List.rev str_intf)
+
 let to_string with_detail (hsh_lnk,fresh) node env =
-	let intf_l,fresh' = 
-		fold_interface
-		(fun i port (cont,fresh) ->
-			let s_i = Environment.site_of_id node.name i env in
-				if not with_detail && (s_i = "_") then (cont,fresh) (*Skipping existential port*)
-				else
-				let (int_state,lnk_state) = port.status
-				and (lifts_int,lifts_lnk) = port.dep 
-				in 
-					let s_int = match int_state with None -> "" | Some x -> ("~"^(Environment.state_of_id node.name i x env))
-					and s_lnk,fresh = 
-						match lnk_state with 
-							| FPtr (u',j) -> 
-								let edge_id,fresh' = 
-									try (Hashtbl.find hsh_lnk (u',j),fresh) 
-									with 
-										| Not_found -> 
-											let addr = try get_address node with Not_found -> invalid_arg "Node.to_string: not allocated"
-											in
-												Hashtbl.add hsh_lnk (addr,i) fresh ;
-												(fresh,fresh+1)
-								in
-									("!"^(string_of_int edge_id),fresh')
-							| Null -> ("",fresh) 
-							| Ptr (node',j) -> 
-								let u' = try get_address node' with Not_found -> invalid_arg "Node.to_string: not allocated"
-								in
-									let edge_id,fresh' = 
-										try (Hashtbl.find hsh_lnk (u',j),fresh) 
-										with 
-											| Not_found -> 
-												let addr = try get_address node with Not_found -> invalid_arg "Node.to_string: not allocated"
-												in
-													Hashtbl.add hsh_lnk (addr,i) fresh ;
-													(fresh,fresh+1)
-									in
-										("!"^(string_of_int edge_id),fresh')
-					in
-						let string_of_lift = 
-							fun inj -> 
-								let (m,c) = Injection.get_coordinate inj in
-								Printf.sprintf "(%s,%s)" (string_of_int m) (string_of_int c)  
-						in
-							let lift_int = Tools.string_of_set string_of_lift LiftSet.fold lifts_int
-							and lift_lnk = Tools.string_of_set string_of_lift LiftSet.fold lifts_lnk
-							in
-								if with_detail then
-									((Printf.sprintf "%s%s%s%s%s" s_i s_int lift_int s_lnk lift_lnk)::cont,fresh)
-								else
-									((Printf.sprintf "%s%s%s" s_i s_int s_lnk)::cont,fresh) 
-		) node ([],fresh) 
+  let ptr_to_string u' i j fresh =
+    let edge_id,fresh' =
+      try (Hashtbl.find hsh_lnk (u',j),fresh)
+      with Not_found ->
+	let addr =
+	  try get_address node
+	  with Not_found ->
+	    invalid_arg "Node.to_string: not allocated"
 	in
-		let s_addr = try string_of_int (get_address node) with Not_found -> "na" in
-		if with_detail then
-			(Printf.sprintf "%s_%s:[%s]" (Environment.name node.name env) s_addr (String.concat "," (List.rev intf_l)) ,fresh')
-		else
-			(Printf.sprintf "%s(%s)" (Environment.name node.name env) (String.concat "," (List.rev intf_l)),fresh') 
+	Hashtbl.add hsh_lnk (addr,i) fresh ;
+	(fresh,fresh+1)
+    in
+    ("!"^(string_of_int edge_id),fresh')
+  in
+  let intf_l,fresh' =
+    fold_interface
+      (fun i port (cont,fresh) ->
+       let s_i = Environment.site_of_id node.name i env in
+       if not with_detail && (s_i = "_")
+       then (cont,fresh) (*Skipping existential port*)
+       else
+	 let (int_state,lnk_state) = port.status in
+	 let (lifts_int,lifts_lnk) = port.dep in
+	 let s_int =
+	   match int_state with
+	     None -> ""
+	   | Some x -> ("~"^(Environment.state_of_id node.name i x env)) in
+	 let s_lnk,fresh =
+	   match lnk_state with
+	   | FPtr (u',j) ->
+	      ptr_to_string u' i j fresh
+	   | Null -> ("",fresh)
+	   | Ptr (node',j) ->
+	      let u' =
+		try get_address node'
+		with Not_found -> invalid_arg "Node.to_string: not allocated"
+	      in
+	      ptr_to_string u' i j fresh
+	 in
+	 let pp_lift f inj =
+	   let (m,c) = Injection.get_coordinate inj in
+	   Format.fprintf f "(%d,%d)" m c in
+	 ((if with_detail then
+	     Format.asprintf
+	       "%s%s%a%s%a" s_i s_int
+	       (Pp.set LiftSet.elements Pp.comma pp_lift) lifts_int s_lnk
+	       (Pp.set LiftSet.elements Pp.comma pp_lift) lifts_lnk
+	   else
+	     Format.sprintf "%s%s%s" s_i s_int s_lnk)::cont,fresh)
+      ) node ([],fresh)
+  in
+  let s_addr = try string_of_int (get_address node) with Not_found -> "na" in
+  if with_detail then
+    (Format.asprintf "%s_%s:[%a]" (Environment.name node.name env) s_addr
+		     (Pp.list Pp.comma Pp.string) (List.rev intf_l) ,fresh')
+  else
+    (Format.asprintf "%s(%a)" (Environment.name node.name env)
+		     (Pp.list Pp.comma Pp.string) (List.rev intf_l),fresh')
 
 let add_dep phi port_list node env =
-	let add lift int_lnk = (fun (x,y) -> if int_lnk = 0 then (LiftSet.add x lift,y) else (x,LiftSet.add y lift))
-	in
-	List.iter
+  let add lift int_lnk (x,y) =
+    if int_lnk = 0 then (LiftSet.add x lift,y) else (x,LiftSet.add y lift)
+  in
+  List.iter
 	(fun (int_lnk,p_k) ->
-		let port_k = node.interface.(p_k) 
-		in 
-		let dep = add phi int_lnk port_k.dep in node.interface.(p_k).dep <- dep
+	 let port_k = node.interface.(p_k) in
+	 let dep = add phi int_lnk port_k.dep in
+	 node.interface.(p_k).dep <- dep
 	) port_list ;
-	node
+  node
 
 
-let iteri f node  = Array.iteri f (interface node) 
+let iteri f node  = Array.iteri f (interface node)
 
 let marshalize node =
 	let f_intf =
@@ -261,44 +270,30 @@ let test (n,i) (int,lnk) port_list =
     with exn ->
       (Debug.tag (Printf.sprintf "Node %d has no site %d" (name n) i)
       ; raise exn) in
-  let b,port_list =
+  let port_list =
     match int with
-    | None -> (true,port_list)
+    | None -> port_list
     (*None in pattern is compatible with anything in the node*)
-    | _ as s -> (s=state,(0,i)::port_list)
+    | s -> if s <> state then raise False else (0,i)::port_list
   in
-  if not b then raise False
-  else
-    match lnk with
-    | Mixture.WLD -> port_list
-    (*None in pattern is compatible with anything in the node*)
-    | Mixture.BND ->
-       (match link with
-	  FPtr _ -> invalid_arg "Node.test"
-	| Ptr _ -> (1,i)::port_list
-	| Null -> raise False)
-    | Mixture.TYPE (sid,nme) ->
-       begin
-	 match link with
-	 | FPtr _ -> invalid_arg "Node.test"
-	 | Ptr (u,j) ->
-	    if (name u = nme) && (j = sid)
-	    then (1,i)::port_list
-	    else raise False
-	 | Null -> raise False
-       end
-    | Mixture.FREE ->
-       (match link with
-	  FPtr _ -> invalid_arg "Node.test"
-	| Ptr _ -> raise False
-	| Null -> (1,i)::port_list)
+  match lnk, link with
+  | _, FPtr _ -> invalid_arg "Node.test"
+  | Mixture.WLD, _ -> port_list
+  (* None in pattern is compatible with anything in the node *)
+  | (Mixture.BND, Ptr _ | Mixture.FREE, Null ) -> (1,i)::port_list
+  | Mixture.TYPE (sid,nme), Ptr (u,j) when (name u = nme) && (j = sid) ->
+     (1,i)::port_list
+  | _, _ -> raise False
 
-let follow (u,i) = 
-	let intf_u = interface u in
-		let (_,lnk) = intf_u.(i).status in
-			match lnk with
-				| Ptr (v,j) -> Some (v,j)
-				| _ -> None
+let follow (u,i) =
+  let intf_u = interface u in
+  let (_,lnk) = intf_u.(i).status in
+  match lnk with
+  | Ptr (v,j) -> Some (v,j)
+  | _ -> None
 
-module NodeHeap = MemoryManagement.Make (struct type t = node let allocate node i = node.address <- (Some i) end)
-
+module NodeHeap =
+  MemoryManagement.Make (struct
+			  type t = node
+			  let allocate node i = node.address <- (Some i)
+			end)
