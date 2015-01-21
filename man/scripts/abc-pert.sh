e54b60c322654f4610e6b88b1713ee7bbc7aa087
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2015-01-22 00:26:07 +0100

    Scripts in man/script fails when something in them fails + aks for fonts in regression tests

--- man/scripts/abc-pert.sh
+++ man/scripts/abc-pert.sh
@@ -1,4 +1,3 @@
-${KAPPABIN}KaSim -i ../kappa/abc-pert.ka -e 100000 -t 300 -p 1000 -o abc-pert.out
-gnuplot abc-pert.gplot 
+${KAPPABIN}KaSim -i ../kappa/abc-pert.ka -e 100000 -t 300 -p 1000 -o abc-pert.out && \
+gnuplot abc-pert.gplot && \
 rm -f  *.out
-
