dcd89630a301aaaf6b11b07dba3f2034c8efa0fc
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2015-02-06 15:39:06 +0100

    if I may insist on escape_label_in_dot

--- KaSa_rep/tools/tools_kasa.ml
+++ KaSa_rep/tools/tools_kasa.ml
@@ -15,37 +15,19 @@ let snd_option x =
    
 
 (** dot output*)
+(* OCaml manual: In particular, if you want a regular expression that
+matches a single backslash character, you need to quote it in the
+argument to regexp (according to the last item of the list above) by
+adding a second backslash. Then you need to quote both backslashes
+(according to the syntax of string constants in OCaml) by doubling
+them again, so you need to write four backslash characters: Str.regexp
+"\\\\".  *)
 let escape_label_in_dot s =
-  Str.global_substitute (Str.regexp "[\"\\]") (function
-  | "\"" -> "\\\""
-  | "\\" -> "\\\\"
-  | x -> x) s
- 
-let make_id_compatible_with_dot_format parameters error string =
-  let tab = 
-    Remanent_parameters.get_make_labels_compatible_with_dot parameters 
-  in 
-  let rec aux pos l = 
-    if pos<0 
-    then l 
-    else
-      let char = String.get string pos in 
-      try 
-	let liste_char = 
-	  Remanent_parameters_sig.CharMap.find char tab 
-	in 
-	aux (pos-1) 
-	  begin 
-	    List.fold_left 
-	      (fun list char -> char::list)
-	      l 
-	      (List.rev liste_char)
-	  end 
-      with 
-      | Not_found -> 
-	aux (pos-1) (char::l) 
-  in 
-  let l = aux (String.length string -1) [] in 
-  error,
-  String.concat "" (List.rev_map (String.make 1) (List.rev l)) (*I fear that this is very costly, but if I am not allowed to use OCaml 4.02*)
+  Str.global_substitute (Str.regexp "[\\\"\\\\]")
+			(fun x -> match Str.matched_string x with
+				  | "\"" -> "\\\""
+				  | "\\" -> "\\\\"
+				  | x -> assert false) s
 
+let make_id_compatible_with_dot_format parameters error string =
+  error,escape_label_in_dot string
