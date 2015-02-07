27a18e42e94b1beba140f147cd7d23da998a5530
feret <feret@ens.fr>
2015-02-07 19:32:44 +0100

    Strating the documentation of KaSa in the manual.

---
+++ man/scripts/kasa_option.sh
@@ -0,0 +1,5 @@
+echo '\\begin{verbatim}\n' > ../generated_img/KaSa_options.txt
+KaSa --help --expert >> ../generated_img/KaSa_options.txt 
+echo '\\end{verbatim}\n' >> ../generated_img/KaSa_options.txt 
+
+
