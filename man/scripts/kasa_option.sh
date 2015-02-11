14e4efd6644e2a3ab88ded4e86ef15dbd0aeda8c
feret <feret@ens.fr>
2015-02-11 06:06:33 +0100

    Use cat instead of echo to dump \begin \end {verbatim} at the beggining and end of the file kasa_options.txt, so as to avoid OS-dependend behavior and fix the bug reported in #96 by rhz.

--- man/scripts/kasa_option.sh
+++
@@ -1,5 +0,0 @@
-echo '\\begin{verbatim}\n' > ../generated_img/KaSa_options.txt
-KaSa --help --expert >> ../generated_img/KaSa_options.txt 
-echo '\\end{verbatim}\n' >> ../generated_img/KaSa_options.txt 
-
-
---
+++ man/scripts/kasa_options.sh
@@ -0,0 +1,5 @@
+cat kasa_options.head > ../generated_img/KaSa_options.txt
+KaSa --help --expert  >> ../generated_img/KaSa_options.txt 
+cat kasa_options.foot >> ../generated_img/KaSa_options.txt 
+
+
