c9b68f2bbd9e97370ea560f54ca339ec348046bb
feret <feret@ens.fr>
2015-02-13 16:59:11 +0100

    Allocate site identifier (for bound binding site) from left to right.

--- KaSa_rep/frontend/preprocess/list_tokens.ml
+++ KaSa_rep/frontend/preprocess/list_tokens.ml
@@ -4,7 +4,7 @@
   * Jérôme Feret, projet Abstraction/Antique, INRIA Paris-Rocquencourt
   * 
   * Creation: 2011, the 17th of January
-  * Last modification: 2014, the 9th of December
+  * Last modification: 2015, the 13th of February
   * * 
   * Number agents, sites, states in ckappa represenations
   *  
@@ -194,7 +194,8 @@ let scan_agent parameters (error,handler) agent =
                             | Ckappa_sig.LNK_VALUE (_,agent',site',_,_) 
                             | Ckappa_sig.LNK_TYPE ((agent',_),(site',_)) -> 
                ( let error,(handler,ag_id')   = declare_agent parameters error handler agent' in 
-                 let error,(handler,_,site_id') = declare_site_with_binding_states parameters (error,handler) ag_id' site' [] in  
+                 let error,(handler,_,site_id) = declare_site_with_binding_states parameters (error,handler) ag_id site_name [] in 
+		 let error,(handler,_,site_id') = declare_site_with_binding_states parameters (error,handler) ag_id' site' [] in  
                  let error,(handler,l1,site_id)  = declare_site_with_binding_states parameters (error,handler) ag_id site_name [Cckappa_sig.Lnk_type (ag_id',site_id')] in                   
                  let error,(handler,l2,site_id') = declare_site_with_binding_states parameters (error,handler) ag_id' site' [Cckappa_sig.Lnk_type (ag_id,site_id)] in
                  let error,handler = 
@@ -275,10 +276,13 @@ let scan_rules parameters a b =
 let scan_compil parameters error compil =
   let parameters = Remanent_parameters.set_trace parameters (local_trace || (Remanent_parameters.get_trace parameters)) in
   let remanent = empty_handler parameters error in 
+  let _ = Printf.fprintf stdout "INIT\n" in 
   let remanent = scan_initial_states parameters remanent compil.Ast.init in 
+  let _ = Printf.fprintf stdout "SIGNATURE\n" in 
   let remanent = scan_declarations parameters remanent compil.Ast.signatures  in 
   let remanent = scan_observables parameters remanent compil.Ast.observables in 
   let remanent = scan_perts parameters remanent compil.Ast.perturbations in 
+  let _ = Printf.fprintf stdout "RULES\n" in 
   let remanent = scan_rules parameters remanent compil.Ast.rules in
   remanent 
    
