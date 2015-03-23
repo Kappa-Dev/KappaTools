31ae33084ff00a01f7353c3470d8f9cdf17c251d
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2015-03-23 22:51:28 +0100

    Travis deals with js

--- dev/update-web.sh
+++ dev/update-web.sh
@@ -4,15 +4,24 @@ set -e
 
 PLAYGROUND=$(mktemp -d -t kappaXXXX)
 git clone --quiet -b master https://${KAPPAGITHUBTOKEN}:@github.com/Kappa-Dev/Kappa-Dev.github.io.git ${PLAYGROUND}
-[ -d ${PLAYGROUND}/docs/KaSim-manual-master/generated_img ] || \
-    mkdir -p ${PLAYGROUND}/docs/KaSim-manual-master/generated_img
-cp man/*.htm man/*.css man/*.png ${PLAYGROUND}/docs/KaSim-manual-master/
-cp -r man/img ${PLAYGROUND}/docs/KaSim-manual-master/
-cp man/generated_img/*.png ${PLAYGROUND}/docs/KaSim-manual-master/generated_img/
+case $1 in
+    native )
+	[ -d ${PLAYGROUND}/docs/KaSim-manual-master/generated_img ] || \
+	    mkdir -p ${PLAYGROUND}/docs/KaSim-manual-master/generated_img
+	cp man/*.htm man/*.css man/*.png ${PLAYGROUND}/docs/KaSim-manual-master/
+	cp -r man/img ${PLAYGROUND}/docs/KaSim-manual-master/
+	cp man/generated_img/*.png ${PLAYGROUND}/docs/KaSim-manual-master/generated_img/
+	;;
+    js )
+	[ -d ${PLAYGROUND}/try ] || \
+	    mkdir@ ${PLAYGROUND}/try
+	cp js/index.html js/JaSim.js ${PLAYGROUND}/try/
+	;;
+esac
 COMMITNAME=$(git show --pretty=oneline -s --no-color)
 cd ${PLAYGROUND}
 git config user.email "kappa-dev@listes.sc.univ-paris-diderot.fr"
 git config user.name "KappaBot"
 git add ${PLAYGROUND}/docs/KaSim-manual-master/ && \
-{ git commit -m "Sync website with Kappa-Dev/KaSim@${COMMITNAME}" && \
-git push -q origin master || echo "No Changes" ; }
+    { git commit -m "Sync website with Kappa-Dev/KaSim@${COMMITNAME}" && \
+    git push -q origin master || echo "No Changes" ; }
