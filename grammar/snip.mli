70c67c6dc10145b7f69ff9a643e8a13c4dea39c7
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2015-05-28 10:20:08 +0200

    forgotten file

---
+++ grammar/snip.mli
@@ -0,0 +1,12 @@
+val connected_components_sum_of_ambiguous_mixture :
+  (string list * (string * string) list) Export_to_KaSim.String2Map.t ->
+  Connected_component.Env.t -> Ast.mixture ->
+  Connected_component.Env.t * Connected_component.t array list
+
+val connected_components_sum_of_ambiguous_rule :
+  (string list * (string * string) list) Export_to_KaSim.String2Map.t ->
+  Connected_component.Env.t -> Ast.mixture -> Ast.mixture ->
+  Connected_component.Env.t *
+    (Connected_component.t array *
+       (Transformations.t list * Transformations.t list))
+      list
