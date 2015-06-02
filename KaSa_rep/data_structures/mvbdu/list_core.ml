25164f809bf2619b9bc491223a6ecb1303ee7de2
quyen <ly.kim.quyen@ens.fr>
2015-06-02 13:57:46 +0200

    space

--- KaSa_rep/data_structures/mvbdu/list_core.ml
+++ KaSa_rep/data_structures/mvbdu/list_core.ml
@@ -9,16 +9,15 @@
     * This library provides primitives to deal associations list 
     *  
     * Copyright 2010 Institut National de Recherche en Informatique et   
-    * en Automatique.  All rights reserved.  This file is distributed     
-    * under the terms of the GNU Library General Public License *)
+   * en Automatique.  All rights reserved.  This file is distributed     
+   * under the terms of the GNU Library General Public License *)
 
 
 let sanity_check = true
 let test_workbench = false 
   
-
 let invalid_arg parameters mh message exn value = 
-     Exception.warn parameters mh (Some "Mvbdu") message exn (fun () -> value)
+  Exception.warn parameters mh (Some "Mvbdu") message exn (fun () -> value)
 
 let get_hash_key list = list.List_sig.id
 
@@ -28,13 +27,14 @@ let get_skeleton prelist =
   match prelist with
     | List_sig.Empty -> List_sig.Empty 
     | List_sig.Cons x -> 
-        List_sig.Cons
+      List_sig.Cons
         { x with 
           List_sig.tail = get_hash_key x.List_sig.tail}
-                
+        
 let build_list (allocate:('a,'b,'c) Sanity_test_sig.g) error handler skeleton cell = 
-      allocate error compare skeleton cell (fun key -> {List_sig.id=key;List_sig.value=cell}) handler  
-  
+  allocate error compare skeleton cell
+    (fun key -> {List_sig.id = key; List_sig.value = cell})
+    handler  
 
 let id_of_list x = x.List_sig.id 
 
