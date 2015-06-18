9b2ace8d13bf030542e75e4bd6e99ed24d6b33ba
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2015-06-18 08:37:29 +0200

    Forgotten Module

---
+++ js/ace.ml
@@ -0,0 +1,21 @@
+
+let get_editor_value () =
+  let res = Js.Unsafe.fun_call
+    (Js.Unsafe.variable
+       "editor")##getValue
+    [| Js.Unsafe.inject () |] in
+  (*Js.to_string*) res
+
+let set_editor_value str =
+  ignore (Js.Unsafe.fun_call
+    (Js.Unsafe.variable
+       "editor")##setValue
+    [| Js.Unsafe.inject str |])
+
+let starter =
+  let script = Dom_html.createScript Dom_html.document in
+  let () =
+    script##text <- Js.string "var editor = ace.edit(\"editor\");
+editor.setTheme(\"ace/theme/eclipse\");" in
+  script
+
