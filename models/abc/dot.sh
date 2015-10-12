8fd11099ed4173e074155bee5e7964e386a1606c
quyen <ly.kim.quyen@ens.fr>
2015-10-12 09:53:53 +0200

    removed folder abc

--- models/abc/dot.sh
+++
@@ -1,2 +0,0 @@
-
-for f in *.dot; do echo ---------------------$f--------------->> $f.ps; ~/Downloads/Software/graphviz/cmd/dot/dot -Tps $f -o 2>&1 | cat >> $f.ps; done
--- models/abc/make.sh
+++
@@ -1,3 +0,0 @@
-rm log.txt;
-for f in  *.ka; do echo ---------------------$f--------------->> log.txt;
-~/KaSim/bin/KaSa $f 2>&1 | cat >> log.txt; done
