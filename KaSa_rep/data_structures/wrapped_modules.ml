ce50c9a00ca1067401cab3d0c65f81cf7859b0e9
Jérôme Feret <feret@ens.fr>
2015-11-16 21:33:04 +0100

    Wrapped versions of Set and Map modules.

---
+++ KaSa_rep/data_structures/wrapped_modules.ml
@@ -0,0 +1,30 @@
+(* versions of the module with logging primitives *)
+(* type are shared among both versions *) 
+module LoggedStringSetMap = Map_wrapper.Make(StringSetMap) 
+module LoggedStringSet = LoggedStringSetMap.Set
+module LoggedStringMap = LoggedStringSetMap.Map
+module LoggedIntSetMap = Map_wrapper.Make(IntSetMap)
+module LoggedIntSet = LoggedIntSetMap.Set
+module LoggedIntMap = LoggedIntSetMap.Map
+module LoggedInt2SetMap = Map_wrapper.Make(Int2SetMap)					  
+module LoggedInt2Set = LoggedInt2SetMap.Set
+module LoggedInt2Map = LoggedInt2SetMap.Map
+module LoggedCharSetMap = Map_wrapper.Make (CharSetMap)
+module LoggedCharSet = LoggedCharSetMap.Set
+module LoggedCharMap = LoggedCharSetMap.Map
+
+(* versions of the module with logging primitives *)
+(* type are hidden, cannot be used with not logged versions *)
+module ParanoStringSetMap = Map_wrapper.Make (SetMap.Make (String))
+module ParanoStringSet = Parano.StringSetMap.Set
+module ParanoStringMap = Parano.StringSetMap.Map
+module ParanoIntSetMap =
+  Map_wrapper.Make (SetMap.Make (struct type t = int let compare = int_compare end))
+module ParanoIntSet = ParanoIntSetMap.Set			
+module ParanoIntMap = ParanoIntSetMap.Map
+module ParanoInt2SetMap = Map_wrapper.Make (SetMap.Make (struct type t = int*int let compare = int_pair_compare end))
+module ParanoInt2Set = ParanoInt2SetMap.Set
+module ParanoInt2Map = ParanoInt2SetMap.Map
+module ParanoCharSetMap = Map_wrapper.Make (SetMap.Make (struct type t = char let compare = compare end))
+module ParanoCharSet = ParanoCharSetMap.Set
+module ParanoCharMap = ParanoCharSetMap.Map
