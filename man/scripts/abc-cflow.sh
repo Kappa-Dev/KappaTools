70b2339645eb00ba8aa5bedaf1f13c7bfb7d5c9f
Jérôme Feret <feret@ens.fr>
2016-01-14 05:34:41 +0100

    I did not want to plot, but if I have too...

--- man/scripts/abc-cflow.sh
+++ man/scripts/abc-cflow.sh
@@ -1,3 +1,3 @@
-"${KAPPABIN}"KaSim ../kappa/abc-cflow.ka -seed 551423834 -t 30 && \
+"${KAPPABIN}"KaSim ../kappa/abc-cflow.ka -seed 551423834 -t 30 -p 10 && \
 dot -Tpng abc-flow_Weakly_0.dot -o ../generated_img/cflow.png && \
 rm -f *.dat *.dot *.txt
