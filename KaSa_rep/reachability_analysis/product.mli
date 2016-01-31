e72fbab26d164f2de3f723585ca958d9f904566a
Jérôme Feret <feret@ens.fr>
2016-01-31 18:20:58 +0100

    Some/More comments are never useless.

--- KaSa_rep/reachability_analysis/product.mli
+++ KaSa_rep/reachability_analysis/product.mli
@@ -4,3 +4,10 @@ functor
 functor
   (Underlying_domain:Analyzer_domain_sig.Domain) ->
 Analyzer_domain_sig.Domain
+
+(** The functor is almost symmetric, but better time performances will be 
+obtained by using New_domain as a single domain and UNderlying_domain as a 
+list of domains (obtained as the result of the product domain.
+That is to say that the time performance will be better with right linear 
+tree *)
+  
