771c758cb2ff1968895316bf83256f5c326b32e7
Jérôme Feret <feret@ens.fr>
2016-03-23 11:20:20 -0400

    Tune output directory for KaSa to the current one.

--- man/scripts/abc.sh
+++ man/scripts/abc.sh
@@ -1,7 +1,7 @@
 "${KAPPABIN}"KaSim ../kappa/abc.ka -seed 751761073 -e 100000 -p 1000 -o abc.out && \
 gnuplot abc.gplot && \
 circo -Tpng flux.dot -o ../generated_img/flux.png && \
-"${KAPPABIN}"KaSa ../kappa/abc.ka && \
+"${KAPPABIN}"KaSa --output-directory - ../kappa/abc.ka && \
 dot -Tpng contact.dot -o ../generated_img/abc_contact.png && \
 dot -Tpng influence.dot -o ../generated_img/abc_influence.png && \
 rm -f  *.out flux.dot contact.dot influence.dot 
