b7f966981d59c9c77042baa564c691c110a1d64f
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2016-04-27 15:40:49 -0400

    version number

--- main/main.ml
+++ main/main.ml
@@ -3,7 +3,7 @@ open Mods
 open State
 open Random_tree
 
-let version = "3.5-121114"
+let version = "3.5-250915"
 
 let usage_msg = "KaSim "^version^": \n"^"Usage is KaSim -i input_file [-e events | -t time] [-p points] [-o output_file]\n"
 let version_msg = "Kappa Simulator: "^version^"\n"
