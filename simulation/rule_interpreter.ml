f7e8b9adf8660674d4a981f7f52e4838a5dea03b
Pierre Boutillier <pierre@hms.harvard.edu>
2016-05-10 09:57:35 -0400

    Expensive typo

--- simulation/rule_interpreter.ml
+++ simulation/rule_interpreter.ml
@@ -380,7 +380,7 @@ let update_edges
 		(root,[(Connected_component.Set.singleton cc,fst root),
 		       Edges.empty_path])::unary_cands,false
 	      else acc) (unary_cands,no_unary') new_obs in
-      if path <> None || exists_root_of_unary_ccs unary_ccs roots''
+      if path = None && exists_root_of_unary_ccs unary_ccs roots''
       then
 	let unaries_to_explore =
 	  List.map (Agent_place.concretize final_inj2graph)
