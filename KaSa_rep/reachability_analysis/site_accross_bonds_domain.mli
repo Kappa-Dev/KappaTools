8fce1e71597af820d1f982a411d1fb3abd16cc5a
Jérôme Feret <feret@ens.fr>
2016-05-18 10:52:11 +0200

    Forgotten to commit the mli files.

---
+++ KaSa_rep/reachability_analysis/site_accross_bonds_domain.mli
@@ -0,0 +1,22 @@
+(*
+  * views_domain.mli
+  * openkappa
+  * Jérôme Feret & Ly Kim Quyen, projet Abstraction, INRIA Paris-Rocquencourt
+  * 
+  * Creation: 2016, the 30th of January
+  * Last modification: 
+  * 
+  * A monolitich domain to deal with all concepts in reachability analysis
+  * This module is temporary and will be split according to different concepts
+  * thanks to the functor Product
+  * 
+  * Copyright 2010,2011,2012,2013,2014,2015,2016 Institut National de Recherche 
+  * en Informatique et en Automatique.  
+  * All rights reserved.  This file is distributed     
+  * under the terms of the GNU Library General Public License *)
+
+(** Abstract domain to over-approximate the set of reachable views *)
+
+
+module Domain:Analyzer_domain_sig.Domain
+    
