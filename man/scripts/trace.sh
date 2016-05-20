9df696cbcf8d29fc8a9abf6770871e196ebb45ec
Jérôme Feret <feret@ens.fr>
2016-05-20 22:27:24 +0200

    Test for pleasing travis.

--- man/scripts/trace.sh
+++ man/scripts/trace.sh
@@ -1,7 +1,7 @@
 "${KAPPABIN}"KaSa ../kappa/protein2x2.ka --compute-local-traces --no-output-directory && \
-    dot -Tpng Agent_trace_P_a1~_a2~_b1~_b2~_g\!.dot -o ../generated_img/trace_raw.png && \
+    dot -Tpng Agent_trace_P_a1~_a2~_b1~_b2~_g!.dot -o ../generated_img/trace_raw.png && \
     rm *.dot && \
     "${KAPPABIN}"KaSa ../kappa/protein2x2.ka --compute-local-traces --no-output-directory --use-macrotransitions-in-local-traces && \
-    dot -Tpng Agent_trace_P_a1~_a2~_b1~_b2~_g\!.dot -o ../generated_img/trace_macro.png && \
+    dot -Tpng Agent_trace_P_a1~_a2~_b1~_b2~_g!.dot -o ../generated_img/trace_macro.png && \
     rm *.dot
 
