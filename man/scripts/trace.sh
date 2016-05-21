c64c2d112ee5007aaaca2a2a38ef0364ff4e9357
Jérôme Feret <feret@ens.fr>
2016-05-21 08:23:19 +0200

    Last chance before requesting Pierre's help.

--- man/scripts/trace.sh
+++ man/scripts/trace.sh
@@ -1,6 +1,6 @@
 "${KAPPABIN}"KaSa ../kappa/protein2x2.ka --compute-local-traces --no-output-directory && \
-    dot -Tpng Agent_trace_P*.dot -o ../generated_img/trace_raw.png && \
-    rm *.dot && \
+    dot -Tpng Agent_trace_P.a1_.a2_.b1_.b2_.g^.dot -o ../generated_img/trace_raw.png && \
+    rm -f *.dot && \
     "${KAPPABIN}"KaSa ../kappa/protein2x2.ka --compute-local-traces --no-output-directory --use-macrotransitions-in-local-traces && \
-    dot -Tpng Agent_trace_P*.dot -o ../generated_img/trace_macro.png && \
-    rm *.dot
+    dot -Tpng Agent_trace_P.a1_.a2_.b1_.b2_.g^.dot -o ../generated_img/trace_macro.png && \
+    rm -f *.dot
