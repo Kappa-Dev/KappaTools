7981ad721511f72a856126c438f61ca32f885237
Jérôme Feret <feret@ens.fr>
2016-06-29 20:18:41 +0200

    Domain instantiation.

---
+++ KaSa_rep/reachability_analysis/domain_selection.ml
@@ -0,0 +1,29 @@
+(**
+  * domain_selection.ml
+  * openkappa
+  * Jérôme Feret & Ly Kim Quyen, projet Abstraction, INRIA Paris-Rocquencourt
+  *
+  * Creation: 2016, the 30th of June
+  * Last modification: Time-stamp: <Jun 29 2016>
+  *
+  * Compute the relations between sites in the BDU data structures
+  *
+  * Copyright 2010,2011,2012,2013,2014,2015,2016 Institut National de Recherche
+  * en Informatique et en Automatique.
+  * All rights reserved.  This file is distributed
+  * under the terms of the GNU Library General Public License *)
+
+module Reachability_analysis =
+  Analyzer.Make
+    (Composite_domain.Make
+       (Product.Product
+          (Parallel_bonds.Domain)
+          (Product.Product
+             (Site_accross_bonds_domain.Domain)
+             (Product.Product
+                (Views_domain.Domain)
+                (Product.Product
+                   (Contact_map_domain.Domain)
+                   (Product.Product
+                      (Agents_domain.Domain)
+                      (Rules_domain.Domain)))))))
