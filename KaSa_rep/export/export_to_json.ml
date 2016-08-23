d18680e9990840f1c4bd887307365cbb92ce7466
Jerome Feret <feret@ens.fr>
2016-08-23 16:21:07 +0200

    Export to json (contact_map, influence_map, dead rules)

---
+++ KaSa_rep/export/export_to_json.ml
@@ -0,0 +1,31 @@
+(**
+  * export_to_KaSim.ml
+  * openkappa
+  * Jérôme Feret, projet Abstraction/Antique, INRIA Paris-Rocquencourt
+  *
+  * Creation: Aug 23 2016
+  * Last modification: Time-stamp: <Aug 23 2016>
+  * *
+  *
+  * Copyright 2010,2011 Institut National de Recherche en Informatique et
+  * en Automatique.  All rights reserved.  This file is distributed
+  * under the terms of the GNU Library General Public License *)
+
+include Export
+let init ?compil () =
+  init ?compil ~called_from:Remanent_parameters_sig.Server ()
+
+let get_contact_map ?accuracy_level:(accuracy_level=Remanent_state.Low) state =
+  let state, cm = get_contact_map ~accuracy_level state in
+  state, contact_map_to_json cm
+
+
+let get_influence_map ?accuracy_level:(accuracy_level=Remanent_state.Low) state =
+  let state, influence_map = get_influence_map ~accuracy_level state in
+  state, influence_map_to_json influence_map
+
+
+
+let get_dead_rules state =
+  let state, rules = get_dead_rules state in
+  state, dead_rules_to_json rules
---
+++ KaSa_rep/export/export_to_json.mli
@@ -0,0 +1,23 @@
+(**
+  * export.mli
+  * openkappa
+  * Jérôme Feret, projet Abstraction/Antique, INRIA Paris-Rocquencourt
+  *
+  * Creation: Aug 23 2016
+  * Last modification: Time-stamp: <Aug 23 2016>
+  * *
+  *
+  * Copyright 2010,2011 Institut National de Recherche en Informatique et
+  * en Automatique.  All rights reserved.  This file is distributed
+  * under the terms of the GNU Library General Public License *)
+
+type state
+
+
+val init:
+?compil:(string Location.annot * Ast.port list, Ast.mixture, string, Ast.rule)
+  Ast.compil -> unit -> state
+
+val get_contact_map: ?accuracy_level:Remanent_state.accuracy_level -> state -> state * Yojson.Basic.json
+val get_influence_map: ?accuracy_level:Remanent_state.accuracy_level -> state -> state * Yojson.Basic.json
+val get_dead_rules: state -> state * Yojson.Basic.json  
