b1bdd11dc71daa9e724bafacf05d4f40704633aa
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2016-10-24 13:46:35 -0400

    RefMan scripts: --batch -> -mode batch

--- man/scripts/abc.sh
+++ man/scripts/abc.sh
@@ -1,4 +1,4 @@
-"${KAPPABIN}"KaSim ../kappa/abc.ka -seed 751761073 -e 100000 -pp 0.25 -o abc.out --batch && \
+"${KAPPABIN}"KaSim ../kappa/abc.ka -seed 751761073 -e 100000 -pp 0.25 -o abc.out -mode batch && \
 gnuplot abc.gplot && \
 circo -Tpng flux.dot -o ../generated_img/flux.png && \
 "${KAPPABIN}"KaSa ../kappa/abc.ka --no-output-directory  && \
