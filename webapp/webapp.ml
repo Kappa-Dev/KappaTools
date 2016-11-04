8236637ccc6d7503735ffb46a26bb975cf233816
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2016-11-04 19:25:21 -0400

    a bit of debbugging for websim

--- webapp/webapp.ml
+++ webapp/webapp.ml
@@ -1,16 +1,5 @@
 module ApiTypes = Api_types_v1_j
 
-open Lwt
-open Cohttp_lwt_unix
-open Cohttp
-open Request
-open Api
-open ApiTypes
-open Conduit_lwt_unix
-open Unix
-open Lwt_log
-open Re
-
 class system_process () : Kappa_facade.system_process =
   object
     method log ?exn (msg : string) =
@@ -30,6 +19,13 @@ let route_handler
   =
   let sytem_process : Kappa_facade.system_process = new system_process () in
   let manager : Api.manager = new Api_runtime.manager sytem_process in
+  let intermediate =
+    (Webapp_common.route_handler
+       ((Route_root.route
+           ~manager:manager
+           ~shutdown_key:shutdown_key)
+        @ Route_sessions.route
+          ~manager:manager)) in
   fun (conn : Cohttp_lwt_unix.Server.conn)
     (request : Cohttp.Request.t)
     (body : Cohttp_lwt_body.t)
@@ -39,10 +35,4 @@ let route_handler
                     ; Webapp_common.request = request
                     ; Webapp_common.body = body }
       in
-      (Webapp_common.route_handler
-         ((Route_root.route
-             ~manager:manager
-             ~shutdown_key:shutdown_key)
-          @ Route_sessions.route
-            ~manager:manager))
-        ~context:context
+      intermediate ~context
