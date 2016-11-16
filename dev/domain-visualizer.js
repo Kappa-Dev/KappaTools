a016fd1926aa53b75ac47093ab8c9d8fe8a98eed
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2016-11-16 00:25:35 -0500

    domain visualizator root by root

--- dev/domain-visualizer.js
+++ dev/domain-visualizer.js
@@ -1,9 +1,29 @@
 var errorsDom = document.getElementById("errors");
+var domainFileDom = document.getElementById("domainFile");
+var rootSelectDom = document.getElementById("rootSelect");
 var svg = d3.select("svg");
 
-var zoom = d3.behavior.zoom().on("zoom", function() {
-    d3.select("svg g").attr("transform", "translate(" + d3.event.translate + ")" +
-	       "scale(" + d3.event.scale + ")");});
+var domain = null
+
+svg.call(d3.behavior.zoom().on("zoom", function() {
+    svg.select("g").attr("transform", "translate(" + d3.event.translate + ")" +
+			 "scale(" + d3.event.scale + ")");}));
+
+var set_rootSelect = function () {
+    while(rootSelectDom.firstChild){
+	rootSelectDom.removeChild(rootSelectDom.firstChild);
+    }
+    domain.elementaries.forEach(function (a) {
+	a.forEach(function (l) {
+	    l.forEach(function (v) {
+		var opt = document.createElement('option');
+		opt.value = v[1];
+		opt.innerHTML = domain.dag[v[1]].content;
+		rootSelectDom.appendChild(opt);
+	    })
+	})
+    });
+}
 
 var dealWithFiles = function (files) {
     if (files.length === 1) {
@@ -14,19 +34,8 @@ var dealWithFiles = function (files) {
 		    errorsDom.innerHTML = error;
 		} else {
 		    errorsDom.innerHTML = null;
-		    var g = new dagreD3.graphlib.Graph().setGraph({});
-		    for (i = 0; i < graph.dag.length; i++) {
-			g.setNode(i, {label: graph.dag[i].content,
-				      style: "fill: #eee"});
-			for (j = 0; j < graph.dag[i].sons.length; j++)
-			    g.setEdge(i,graph.dag[i].sons[j].dst,
-				      {label: JSON.stringify(graph.dag[i].sons[j].nav),
-				       style: "fill: none; stroke: #222; stroke-width: 1.5px"});
-		    }
-		    var render = new dagreD3.render();
-		    render(svg,g);
-		    svg.call(zoom);
-		    svg.attr('height', g.graph().height + 20);
+		    domain = graph;
+		    set_rootSelect();
 		}
 	    })
 	}
@@ -34,5 +43,26 @@ var dealWithFiles = function (files) {
     }
 }
 
-var domainFileDom = document.getElementById("domainFile");
+var addToGraph = function (cache,g,i) {
+    if (cache.every(function (v) { return v !== i ; })) {
+	g.setNode(i, {label: domain.dag[i].content,
+		      style: "fill: #eee"});
+	domain.dag[i].sons.forEach(function (s) {
+	    addToGraph(cache,g,s.dst);
+	    g.setEdge(i,s.dst,
+		      {label: JSON.stringify(s.nav),
+		       style: "fill: none; stroke: #222; stroke-width: 1.5px"});
+	});
+	cache.push(i);
+    }
+}
+
 domainFileDom.onchange = function() {dealWithFiles(domainFileDom.files)};
+
+rootSelectDom.onchange = function () {
+    var g = new dagreD3.graphlib.Graph().setGraph({});
+    addToGraph([],g,Number(rootSelectDom.value));
+    var render = new dagreD3.render();
+    render(svg,g);
+    svg.attr('height', g.graph().height + 20);
+};
