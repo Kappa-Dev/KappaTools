99beb84e591d0848f8ed011d868535eb4c4714a0
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2016-12-07 15:09:39 -0500

    Dead code

--- dataStructures/mods.mli
+++ dataStructures/mods.mli
@@ -4,19 +4,19 @@ val int_compare : int -> int -> int
 val int_pair_compare : (int*int) -> (int*int) -> int
 
 module StringSetMap : SetMap.S with type elt = string
-module StringSet : SetMap.Set with type elt = string
-module StringMap : SetMap.Map with type elt = string and type set = StringSet.t
+module StringSet = StringSetMap.Set
+module StringMap = StringSetMap.Map
 
 module IntSetMap : SetMap.S with type elt = int
-module IntSet : SetMap.Set with type elt = int
-module IntMap : SetMap.Map with type elt = int and type set = IntSet.t
+module IntSet = IntSetMap.Set
+module IntMap = IntSetMap.Map
 
 module Int2SetMap : SetMap.S with type elt = int*int
-module Int2Set : SetMap.Set with type elt = int*int
-module Int2Map : SetMap.Map with type elt = int*int and type set = Int2Set.t
+module Int2Set = Int2SetMap.Set
+module Int2Map = Int2SetMap.Map
 
 module CharSetMap : SetMap.S with type elt = char
-module CharSet : SetMap.Set with type elt = char
-module CharMap : SetMap.Map with type elt = char and type set = CharSet.t
+module CharSet = CharSetMap.Set
+module CharMap = CharSetMap.Map
 
 module DynArray : GenArray.GenArray
