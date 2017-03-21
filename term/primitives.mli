digideskio/KaSim

Constraints in rules

term/primitives.mli
@@ -33,6 +33,7 @@ type elementary_rule = {
   injected_tokens : (Alg_expr.t * int) list;
   syntactic_rule : int;
   (** [0] means generated for perturbation. *)
+  blacklist : Connected_component.t list;
   instantiations : Instantiation.abstract Instantiation.event;
 }
 

Kappa-Dev/KappaTools

term/primitives.mli
@@ -52,6 +52,7 @@ type elementary_rule = {
   delta_tokens : (Alg_expr.t Locality.annot * int) list;
   syntactic_rule : int;
   (** [0] means generated for perturbation. *)
+  blacklist : Pattern.id list;
   instantiations : Instantiation.abstract Instantiation.event;
   (** In the reverse order on purpose so that we rev_map when we
       concretize *)