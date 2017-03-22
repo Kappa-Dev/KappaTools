140d19dddad5381f53a0cadc74f980c1a717637a
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2017-03-22 11:51:57 -0400

    Cosmetic improvements
    Allow more reply to fix #370
    Document replay

--- siteGraphs/replay.mli
+++ siteGraphs/replay.mli
@@ -8,3 +8,6 @@ type state = {
 val init_state : with_connected_components:bool -> state
 
 val do_step : Signature.s -> state -> Trace.step -> state * (int * int) option
+(** @return the new state and, if the step was an unary instance of a
+    binary rule, the id of the rule and the distance between its 2
+    connected patterns. *)
