037b728ca3ae1c3b4080af5d10d104e381421b8a
feret <feret@ens.fr>
2017-03-24 18:14:37 +0100

    missing files

---
+++ term/patterns_extra.mli
@@ -0,0 +1,29 @@
+val raw_mixture_to_species:
+  ?parameters:Remanent_parameters_sig.parameters ->
+  ?signature:Signature.s ->
+  Pattern.PreEnv.t ->
+  Raw_mixture.t ->
+  (int * int) list ->
+  Pattern.PreEnv.t * Pattern.cc * Pattern.id
+
+val species_to_raw_mixture:
+  ?parameters:Remanent_parameters_sig.parameters ->
+  Signature.s ->
+  Pattern.cc -> (Raw_mixture.t * (int * int) list) option
+
+val pattern_to_mixture:
+  ?parameters:Remanent_parameters_sig.parameters ->
+  Signature.s ->
+    Pattern.cc -> LKappa.rule_mixture option
+
+val pattern_id_to_mixture:
+  ?parameters:Remanent_parameters_sig.parameters ->
+  Model.t ->
+  Signature.s ->
+  Pattern.id ->
+  LKappa.rule_mixture option
+
+val raw_mixture_to_lkappa_rule: Raw_mixture.t -> LKappa.rule
+val species_to_lkappa_rule:
+  Remanent_parameters_sig.parameters ->
+  Model.t -> Pattern.t -> LKappa.rule
