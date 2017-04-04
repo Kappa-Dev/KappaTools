3b011d7a644c37417268259648fee0dd1092028d
feret <feret@ens.fr>
2017-04-04 16:10:58 -0400

    Live from Walter's talk

--- KaSa_rep/main/get_option.ml
+++ KaSa_rep/main/get_option.ml
@@ -4,7 +4,7 @@
   * Jérôme Feret, projet Abstraction, INRIA Paris-Rocquencourt
   *
   * Creation: 18/12/2010
-  * Last modification: Time-stamp: <Mar 15 2017>
+  * Last modification: Time-stamp: <Apr 04 2017>
   * *
   * primitive to parse command-line options
   *
@@ -49,9 +49,9 @@ let options =
 
       "--influence-map-accuracy-level",
       (Choice
-         (["Low","Ignore gluing compatibility";
-           "Medium","Ignore reachable states";
-           "High","Take into account reachable states" ],
+         (["Indirect","Ignore gluing compatibility";
+           "Direct","Ignore reachable states";
+           "Realisable","Take into account reachable states" ],
           Config.influence_map_accuracy_level)),
       "Tune the accuracy level of the influence map",
       ["5_Influence_map"],
