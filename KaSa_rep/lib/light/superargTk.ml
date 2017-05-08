9cd45413af2cababe0977a9ecdc2ec0e6ae6e939
Jérôme Feret <feret@ens.fr>
2017-05-08 15:45:48 +0200

    setting the title of the GUI for KaDe, dealing with negation of the options with a single dash properly

--- KaSa_rep/lib/light/superargTk.ml
+++ KaSa_rep/lib/light/superargTk.ml
@@ -3,23 +3,24 @@
    Copyright (C) Antoine Mine' 2006
    Light Version (Jerome Feret)
    This LIGHT version does not require labltk
-   
+
  *)
 
 exception Exit of string list
 
 
-	   
+
 (* MAIN *)
 (* **** *)
 
-let parse (a:Superarg.t) (def:string list ref) =
+let parse ?title (a:Superarg.t) (def:string list ref) =
   Superarg.check a;
   (* drop the first command-line argument: it is the executable name *)
   let args = List.tl (Array.to_list Sys.argv) in
   (* if no argument or "--gui" given, launch the gui, otherwise, parse args *)
   let rem =
     if args=[] || List.exists ((=) "--gui") args
-    then Superarg.parse_list a ["--help"] else Superarg.parse_list a args
+    then Superarg.parse_list ?title a ["--help"]
+    else Superarg.parse_list ?title a args
   in
   if rem<>[] then def := rem
