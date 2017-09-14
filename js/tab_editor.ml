2ecda0f5087c2d633b4123d79e4006182a44aca8
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2017-09-14 00:13:44 -0400

    Dead rules on the left of the editor
    but you don't see them for now because the range returned by KaSa is dummy_loc...

--- js/tab_editor.ml
+++ js/tab_editor.ml
@@ -7,6 +7,7 @@
 (******************************************************************************)
 
 module Html = Tyxml_js.Html5
+open Lwt.Infix
 
 let navli () = ReactiveData.RList.empty
 let rightsubpanel_id : string = "rightsubpanel"
@@ -27,7 +28,6 @@ let rightsubpanel () =
     [Ui_common.navtabs "subnavtab"
        [ "contact_map", (Tab_contact_map.navli ())
        ; "influences", (Tab_influences.navli ())
-       ; "dead_rules", (Tab_dead_rules.navli ())
        ; "constraints", (Tab_constraints.navli ())
        ];
      Ui_common.navcontent
@@ -35,7 +35,6 @@ let rightsubpanel () =
        []
        [ "contact_map", [], (Tab_contact_map.content ())
        ; "influences",  [], (Tab_influences.content ())
-       ; "dead_rules",  [], (Tab_dead_rules.content ())
        ; "constraints", [], (Tab_constraints.content ())
        ]]
 
@@ -59,19 +58,49 @@ let childs_hide b =
   if b then
     let () = Tab_contact_map.parent_hide () in
     let () = Tab_influences.parent_hide () in
-    let () = Tab_dead_rules.parent_hide () in
     Tab_constraints.parent_hide ()
   else
     let () = Tab_contact_map.parent_shown () in
     let () = Tab_influences.parent_shown () in
-    let () = Tab_dead_rules.parent_shown () in
     Tab_constraints.parent_shown ()
+
+let init_dead_rules () =
+  React.S.l1
+    (fun _ ->
+       State_project.with_project
+         ~label:__LOC__
+         (fun (manager : Api.concrete_manager) ->
+            (Lwt_result.map
+               (fun dead_json ->
+                  let list = Public_data.dead_rules_of_json dead_json in
+                  let warnings =
+                    List.fold_left
+                      (fun acc rule ->
+                         let message_text =
+                           "Dead rule "^
+                           if rule.Public_data.rule_label <> ""
+                           then (" '"^rule.Public_data.rule_label^"'")
+                           else if rule.Public_data.rule_ast <> ""
+                           then rule.Public_data.rule_ast
+                           else string_of_int rule.Public_data.rule_id in
+                         {
+                           Api_types_t.message_severity = `Warning;
+                           Api_types_t.message_range =
+                             Some rule.Public_data.rule_position;
+                           Api_types_t.message_text;
+                         } :: acc) [] list in
+                  State_error.add_error __LOC__ warnings)
+               manager#get_dead_rules) >>=
+            fun out -> Lwt.return (Api_common.result_lift out)
+         )
+    )
+    State_project.model
 
 let onload () =
   let () = Subpanel_editor.onload () in
+  let _ = init_dead_rules () in
   let () = Tab_contact_map.onload () in
   let () = Tab_influences.onload () in
-  let () = Tab_dead_rules.onload () in
   let () = Tab_constraints.onload () in
   let _ = React.S.map childs_hide Subpanel_editor.editor_full in
   let () = Common.jquery_on
@@ -81,8 +110,8 @@ let onload () =
   ()
 
 let onresize () : unit =
+  let () = Subpanel_editor.onresize () in
   let () = Tab_contact_map.onresize () in
   let () = Tab_influences.onresize () in
-  let () = Tab_dead_rules.onresize () in
   let () = Tab_constraints.onresize () in
   ()
