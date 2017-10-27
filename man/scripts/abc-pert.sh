4a25cf490838b2c736456f13f3d82ed7c2536e8c
Pierre Boutillier <pierre_boutillier@hms.harvard.edu>
2017-10-26 21:34:02 -0400

    make doc fix

--- man/scripts/abc-pert.sh
+++ man/scripts/abc-pert.sh
@@ -1,3 +1,3 @@
-"${KAPPABIN}"KaSim ../../models/abc-pert.ka -seed 822295616 -l 90 -p 0.3 -o abc-pert.csv -mode batch && \
+"${KAPPABIN}"KaSim ../../models/abc-pert.ka -seed 822295616 -l 90 -p 0.3 -o abc-pert.csv -mode batch -syntax 4 && \
 gnuplot abc-pert.gplot && \
 rm -f  *.csv inputs.ka
