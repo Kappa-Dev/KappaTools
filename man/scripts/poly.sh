fbaaf4af710be8b2c19ecd07eca53094972418a0
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2017-10-27 22:34:40 -0400

    poly.ka in syntax V4

--- man/scripts/poly.sh
+++ man/scripts/poly.sh
@@ -1,3 +1,3 @@
-"${KAPPABIN}"KaSim ../../models/poly.ka -seed 588300472 -u e -p 0 -l 10000 -mode batch -syntax 3 && \
+"${KAPPABIN}"KaSim ../../models/poly.ka -seed 588300472 -u e -p 0 -l 10000 -mode batch && \
 dot -Tpng snap*.dot -o ../generated_img/poly.png && \
 rm -f *.dot inputs.ka
