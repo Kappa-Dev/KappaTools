2e6df0e0f3f02bf6f17d37480d92ad4f9b809321
Pierre Boutillier <pierre_boutillier@hms.harvard.edu>
2017-11-08 20:59:54 -0500

    Me trying to compensate (unsuccessfully) my stupidity

--- python/tests/test_kappa_client.py
+++ python/tests/test_kappa_client.py
@@ -8,13 +8,14 @@ import random
 import string
 import time
 import uuid
-from os.path import dirname, join, normpath, pardir, abspath
+
+from os import path
+import sys
 
 import kappy
-from util import run_nose
 
-KASIM_DIR = normpath(join(dirname(abspath(__file__)), *([pardir]*2)))
-KAPPA_BIN = os.path.join(KASIN_DIR,"bin")
+KASIM_DIR = path.normpath(path.join(path.dirname(path.abspath(__file__)), *([path.pardir]*2)))
+KAPPA_BIN = path.join(KASIM_DIR,"bin")
 
 def _get_id(name):
     return "%s-%s" % (name, uuid.uuid1())
@@ -45,7 +46,7 @@ class _KappaClientTest(unittest.TestCase):
         except kappy.KappaError:
             pass
 
-    def parse_multiple_files(self):
+    def test_parse_multiple_files(self):
         runtime = self.getRuntime(project_id=_get_id('test_proj'))
         file_1_id = _get_id("file1.ka")
         file_2_id = _get_id("file2.ka")
@@ -60,7 +61,7 @@ class _KappaClientTest(unittest.TestCase):
                 data_2 = file_2.read()
                 file_2_metadata = kappy.FileMetadata(file_2_id,2)
                 file_2_object = kappy.File(file_2_metadata,data_2)
-                runtime.file_create(file_1_object)
+                runtime.file_create(file_2_object)
                 runtime.project_parse()
                 with self.assertRaises(kappy.KappaError):
                     runtime.file_create(file_2_object)
@@ -68,7 +69,7 @@ class _KappaClientTest(unittest.TestCase):
                 self.assertIn(file_1_id,file_names)
                 self.assertIn(file_2_id,file_names)
 
-    def test_run_simulationd(self):
+    def test_run_simulation(self):
         project_id = str(uuid.uuid1())
         runtime = self.getRuntime(project_id)
         file_id = str(uuid.uuid1())
@@ -137,15 +138,17 @@ class RestClientTest(_KappaClientTest):
     """ Integration test for kappa client"""
     def __init__(self, *args, **kwargs):
         """ initalize test by launching kappa server """
-        self.websim = join(KAPPA_BIN, "WebSim")
+        self.websim = path.join(KAPPA_BIN, "WebSim")
         self.key = self.generate_key()
         self.port = 6666
         self.endpoint = "http://127.0.0.1:{0}".format(self.port)
+        super(RestClientTest, self).__init__(*args, **kwargs)
+
 
     def setUp(self):
         """ set up unit test by launching client"""
         print("Starting server")
-        subproccess.Popen([
+        subprocess.Popen([
             self.websim,
             '--shutdown-key', self.key,
             '--port', str(self.port),
@@ -189,6 +192,7 @@ class StdClientTest(_KappaClientTest):
         return(kappy.KappaStd(KAPPA_BIN))
 
 if __name__ == '__main__':
-    fpath = os.path.abspath(__file__)
-    print("Running nose for package: %s" % fname)
-    return nose.run(argv=[sys.argv[0], fpath] + sys.argv[1:])
+    module_name = sys.modules[__name__].__file__
+    print("Running nose for package: %s" % module_name)
+
+    result = nose.run(argv=[sys.argv[0], module_name] + sys.argv[1:])
