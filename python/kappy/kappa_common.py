0ef2e0b29ecdb90c23084f283d86072387bde43d
pagreene <patrick.anton.greene@gmail.com>
2017-11-14 17:57:19 -0500

    Fix minor typo.

--- python/kappy/kappa_common.py
+++ python/kappy/kappa_common.py
@@ -283,7 +283,7 @@ class KappaApi(ABC):
         return ret_data
 
     def set_default_sim_param(self, *args, **kwargs):
-        """Set the simulatin default simulation parameters.
+        """Set the simulation default simulation parameters.
 
         You can pass one of two things in as input:
         - a kappa_common.SimulationParameter instance
