4a5ba669f29320b663aade22736262dacfd8cc91
pagreene <patrick.anton.greene@gmail.com>
2017-11-14 16:21:01 -0500

    Add tests for doc behavior.

--- python/tests/test_std.py
+++ python/tests/test_std.py
@@ -14,5 +14,12 @@ class StdClientTest(_KappaClientTest):
         return kappy.KappaStd(BIN_DIR)
 
 
+def test_docs():
+    kappa_std_doc = kappy.KappaStd.file_create.__doc__
+    kappa_abst_doc = kappy.kappa_common.KappaApi.file_create.__doc__
+    assert kappa_std_doc is not None and kappa_std_doc == kappa_abst_doc, \
+        "Doc fixing failed: parent method doc not inheritted by KappaStd."
+
+
 if __name__ == '__main__':
     run_nose(__file__)
