f64f68139c1e3c1a2c9e3bffc1f0799890893f7e
pagreene <patrick.anton.greene@gmail.com>
2017-11-14 17:44:03 -0500

    Ensure default file_id for added model is unique.

--- python/kappy/kappa_common.py
+++ python/kappy/kappa_common.py
@@ -268,12 +268,16 @@ class KappaApi(ABC):
 
     def add_model_string(self, model_str, position=1, file_id=None):
         """Add a kappa model given in a string to the project."""
+        if file_id is None:
+            file_id = self.make_unique_id('inlined_input')
         ret_data = self.file_create(File.from_string(model_str, position,
                                                      file_id))
         return ret_data
 
     def add_model_file(self, model_fpath, position=1, file_id=None):
         """Add a kappa model from a file at given path to the project."""
+        if file_id is None:
+            file_id = self.make_unique_id('file_input')
         ret_data = self.file_create(File.from_file(model_fpath, position,
                                                    file_id))
         return ret_data
