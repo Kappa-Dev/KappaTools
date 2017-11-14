5f3a74069735102697bf970f88e9c313287ad94c
pagreene <patrick.anton.greene@gmail.com>
2017-11-14 17:06:05 -0500

    Add doc strings to a couple methods.

--- python/kappy/kappa_common.py
+++ python/kappy/kappa_common.py
@@ -262,11 +262,13 @@ class KappaApi(ABC):
         return child_class
 
     def add_model_string(self, model_str, position=1, file_id=None):
+        """Add a kappa model given in a string to the project."""
         ret_data = self.file_create(File.from_string(model_str, position,
                                                      file_id))
         return ret_data
 
     def add_model_file(self, model_fpath, position=1, file_id=None):
+        """Add a kappa model from a file at given path to the project."""
         ret_data = self.file_create(File.from_file(model_fpath, position,
                                                    file_id))
         return ret_data
