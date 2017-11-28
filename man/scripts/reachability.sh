73b40ed738aa424961be030b16b498e71323fffb
Jérôme Feret <feret@ens.fr>
2017-11-28 18:23:46 +0100

    script: reachability.sh for the manual

--- man/scripts/reachability.sh
+++ man/scripts/reachability.sh
@@ -5,11 +5,11 @@ cat kasa_options.head > LOG_medium_KO.txt && \
 cat kasa_options.head > LOG_high_init.txt && \
 cat kasa_options.head > LOG_high_rule.txt && \
 cat kasa_options.head > LOG_full.txt && \
-"${KAPPABIN}"KaSa -syntax 3 --reset-all --compute-reachability-analysis ../kappa/reachability.ka | fold -w 61 -s >> LOG_low.txt && \
-"${KAPPABIN}"KaSa -syntax 3 --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis Mute | fold -w 61 -s >> LOG_mute.txt && \
-"${KAPPABIN}"KaSa -syntax 3 --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis Medium > LOG_medium.txt && \
-"${KAPPABIN}"KaSa -syntax 3 --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis High >> LOG_high.txt && \
-"${KAPPABIN}"KaSa -syntax 3 --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis Full > LOG_pre_full.txt && \
+"${KAPPABIN}"KaSa --reset-all --compute-reachability-analysis ../kappa/reachability.ka | fold -w 61 -s >> LOG_low.txt && \
+"${KAPPABIN}"KaSa --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis Mute | fold -w 61 -s >> LOG_mute.txt && \
+"${KAPPABIN}"KaSa --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis Medium > LOG_medium.txt && \
+"${KAPPABIN}"KaSa --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis High >> LOG_high.txt && \
+"${KAPPABIN}"KaSa --reset-all --compute-reachability-analysis ../kappa/reachability.ka --verbosity-level-for-reachability-analysis Full > LOG_pre_full.txt && \
 cat kasa_options.foot >> LOG_mute.txt && \
 cat kasa_options.foot >> LOG_low.txt && \
 grep -h --before-context 1 -m 1 "is satisfied" LOG_medium.txt | fold -w 81 -s >> LOG_medium_OK.txt && \
