5897306d4e0afc0e9e50d52823c3864b521fd526
Jerome Feret <feret@ens.fr>
2017-11-30 18:08:32 +0100

    polymer detection, still todo the dimer example

--- man/scripts/scc.sh
+++ man/scripts/scc.sh
@@ -1,15 +1,18 @@
-"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc2.ka  --no-output-directory  && \
-dot -Tpng contact.dot -o ../generated_img/scc2_contact.png && \
+"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_abc.ka  --no-output-directory  && \
+dot -Tpng contact.dot -o ../generated_img/scc_abc_contact.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html && \
-"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc3.ka  --no-output-directory  && \
-dot -Tpng contact.dot -o ../generated_img/scc3_contact_low.png && \
+"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_relations.ka  --no-output-directory  --polymer-detection-accuracy-level Low && \
+dot -Tpng contact.dot -o ../generated_img/scc_relations_contact_low.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html && \
-"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc4.ka  --no-output-directory  && \
+"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_dimer.ka  --no-output-directory  --polymer-detection-accuracy-level Low && \
 dot -Tpng contact.dot -o ../generated_img/scc4_contact_low.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html && \
-"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc3.ka  --no-output-directory  && \
-dot -Tpng contact.dot -o ../generated_img/scc3_contact_low.png && \
+"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_relations.ka  --no-output-directory  && \
+dot -Tpng contact.dot -o ../generated_img/scc_relations_contact_high.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html
-"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc4.ka  --no-output-directory  && \
-dot -Tpng contact.dot -o ../generated_img/scc4_contact_low.png && \
+"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_dimer.ka  --no-output-directory  && \
+dot -Tpng contact.dot -o ../generated_img/scc4_contact_high.png && \
+rm -f  *.csv flux.dot contact.dot profiling.html
+"${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_dimer.ka --no-double-bonds-domain --no-output-directory  && \
+dot -Tpng contact.dot -o ../generated_img/scc4_contact_high_no_double_bonds.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html
