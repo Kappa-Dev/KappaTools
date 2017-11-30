941c1b9bc922f0d9e9b5f8827532d4ef3fa00de9
Jérôme Feret <feret@ens.fr>
2017-11-30 22:00:56 +0100

    polymer detection: third example

--- man/scripts/scc.sh
+++ man/scripts/scc.sh
@@ -5,14 +5,14 @@ rm -f  *.csv flux.dot contact.dot profiling.html && \
 dot -Tpng contact.dot -o ../generated_img/scc_relations_contact_low.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html && \
 "${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_dimer.ka  --no-output-directory  --polymer-detection-accuracy-level Low && \
-dot -Tpng contact.dot -o ../generated_img/scc4_contact_low.png && \
+dot -Tpng contact.dot -o ../generated_img/scc_dimer_contact_low.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html && \
 "${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_relations.ka  --no-output-directory  && \
 dot -Tpng contact.dot -o ../generated_img/scc_relations_contact_high.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html
 "${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_dimer.ka  --no-output-directory  && \
-dot -Tpng contact.dot -o ../generated_img/scc4_contact_high.png && \
+dot -Tpng contact.dot -o ../generated_img/scc_dimer_contact_high.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html
 "${KAPPABIN}"KaSa --compute-potential-cycles ../kappa/scc_dimer.ka --no-double-bonds-domain --no-output-directory  && \
-dot -Tpng contact.dot -o ../generated_img/scc4_contact_high_no_double_bonds.png && \
+dot -Tpng contact.dot -o ../generated_img/scc_dimer_contact_high_no_double_bonds.png && \
 rm -f  *.csv flux.dot contact.dot profiling.html
