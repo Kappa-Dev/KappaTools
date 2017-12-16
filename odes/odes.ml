Kappa-Dev/KappaTools

commit 489c6ce6491e93aa8849445f1d2030215fd53e53

restore (breakable) space after agent separators

odes/odes.ml
@@ -1,6 +1,6 @@
 (** Network/ODE generation
   * Creation: 15/07/2016
-  * Last modification: Time-stamp: <Dec 08 2017>
+  * Last modification: Time-stamp: <Dec 15 2017>
 *)
 
 let local_trace = false
@@ -757,7 +757,7 @@ struct
     else
       remanent
 
-  let initial_network ?max_size ~dotnet parameters compil network initial_states rules =
+  let initial_network ?max_size parameters compil network initial_states rules =
     let network =
       {network with has_empty_lhs = Some false}
     in
@@ -782,7 +782,7 @@ struct
            ([], network)
            initial_states) rules
     in
-    if dotnet
+    if I.do_we_allow_empty_lhs compil
     then
       match network.has_empty_lhs with
       | None ->
@@ -819,7 +819,7 @@ struct
     compare_reaction (fst a) (fst b)
 
 
-  let compute_reactions ?max_size ~smash_reactions ~dotnet parameters compil network rules initial_states =
+  let compute_reactions ?max_size ~smash_reactions parameters compil network rules initial_states =
     (* Let us annotate the rules with cc decomposition *)
     let n_rules = List.length rules in
     let cache = network.cache in
@@ -865,7 +865,7 @@ struct
     in
     let rules = List.rev rules_rev in
     let to_be_visited, network =
-      initial_network ?max_size ~dotnet
+      initial_network ?max_size
         parameters compil network initial_states rules
     in
     let network =
@@ -1624,7 +1624,7 @@ struct
     | None -> "none"
     | Some b -> string_of_bool b
 
-  let network_from_compil ?max_size ~dotnet ~smash_reactions ~ignore_obs parameters compil network =
+  let network_from_compil ?max_size ~smash_reactions ~ignore_obs parameters compil network =
     let () = Format.printf "+ generate the network... @." in
     let rules = I.get_rules compil in
     let () = Format.printf "\t -initial states @." in
@@ -1635,7 +1635,7 @@ struct
       Format.printf "\t -saturating the set of molecular species @."
     in
     let network =
-      compute_reactions ?max_size ~dotnet ~smash_reactions parameters compil network rules initial_state
+      compute_reactions ?max_size ~smash_reactions parameters compil network rules initial_state
     in
     let () = Format.printf "\t -tokens @." in
     let network = convert_tokens compil network in
@@ -3076,10 +3076,10 @@ struct
                              (Mods.DynArray.get network.species_tab k))
                       ) k, Some "substance"
                   | Loggers.DOTNET ->
-                    let dotnet = true in
+                    let compil = I.to_dotnet compil in
                     (string_of_int k),
                     Format.asprintf "%a"
-                      (fun log k -> I.print_chemical_species ~dotnet ~compil
+                      (fun log k -> I.print_chemical_species ~compil
                         log
                         (fst (Mods.DynArray.get network.species_tab k))
                       ) k, Some ""

KaSa_rep/config/remanent_parameters.ml
@@ -4,7 +4,7 @@
   * Jérôme Feret, projet Abstraction/Antique, INRIA Paris-Rocquencourt
   *
   * Creation: 2010, the 19th of December
-  * Last modification: Time-stamp: <Dec 14 2017>
+  * Last modification: Time-stamp: <Dec 15 2017>
   * *
   * Configuration parameters which are passed through functions computation
   *
@@ -449,7 +449,7 @@ let get_agent_sep_comma_symbol_1 symbol = symbol.Symbol_table.agent_sep_comma
 let get_agent_sep_plus_symbol_1 symbol = symbol.Symbol_table.agent_sep_plus
 let get_agent_sep_dot_symbol_1 symbol = symbol.Symbol_table.agent_sep_dot
 let get_btype_sep_symbol_1 symbol = symbol.Symbol_table.btype_sep
-let get_site_sep_comma_symbol_1 symbol = symbol.Symbol_table.site_sep_comma
+let get_site_sep_comma_symbol_1 symbol = symbol.Symbol_table.site_sep
 let get_ghost_agent_symbol_1 symbol = symbol.Symbol_table.ghost_agent
 let get_do_we_show_ghost_1 symbol = symbol.Symbol_table.show_ghost
 let get_uni_arrow_symbol_1 symbol = symbol.Symbol_table.uni_arrow

KaSa_rep/config/symbol_table.ml
@@ -1,32 +1,38 @@
 type symbol_table =
   {
-    bound : string;
-    open_binding_state : string;
-    close_binding_state : string;
-    link_to_any : string;
-    link_to_some : string;
-    internal_state_symbol : string;
-    free : string;
-    open_internal_state : string;
-    close_internal_state : string;
-    at : string ;
     agent_open : string ;
     agent_close : string ;
-    site_sep_comma : string ;
-    btype_sep : string ;
     agent_sep_comma : string ;
     agent_sep_dot : string ;
     agent_sep_plus : string ;
+    compact_agent_sep_comma: bool ;
+    compact_agent_sep_dot: bool ;
+    compact_agent_sep_plus: bool ;
     ghost_agent : string ;
     show_ghost : bool ;
+    internal_state_symbol : string;
+    open_internal_state : string;
+    close_internal_state : string;
+    open_internal_state_mod : string;
+    close_internal_state_mod : string;
+    internal_state_mod_symbol: string;
+    open_binding_state : string;
+    close_binding_state : string;
+    open_binding_state_mod: string;
+    binding_state_mod_symbol: string;
+    close_binding_state_mod: string;
+    free : string;
+    bound : string;
+    link_to_any : string;
+    link_to_some : string;
+    at : string ;
+    site_sep : string ;
+    btype_sep : string ;
     uni_arrow : string ;
     rev_arrow : string ;
     bi_arrow : string ;
     uni_arrow_nopoly : string ;
-    dotnet_site_sep : string ;
-    dotnet_agent_sep_comma : string ;
-    dotnet_agent_sep_dot : string ;
-    sotnet_agent_set_plus : string
+    breakable : bool ;
   }
 
 let symbol_table_V3 =
@@ -39,29 +45,37 @@ let symbol_table_V3 =
     internal_state_symbol = "~";
     open_internal_state = "";
     close_internal_state = "";
+    open_internal_state_mod = "";
+    close_internal_state_mod = "";
+    internal_state_mod_symbol = "/";
+    open_binding_state_mod = "";
+    close_binding_state_mod = "";
+    binding_state_mod_symbol = "";
     free = "";
     at = "." ;
     agent_open = "(" ;
     agent_close =  ")" ;
     agent_sep_comma = "," ;
-    agent_sep_plus = "+" ;
-    agent_sep_dot = "." ;
+    agent_sep_plus = "," ;
+    agent_sep_dot = "," ;
+    compact_agent_sep_comma = false ;
+    compact_agent_sep_plus = false ;
+    compact_agent_sep_dot = false ;
     btype_sep = ".";
-    site_sep_comma = "," ;
+    site_sep = "," ;
     ghost_agent = "." ;
     show_ghost = false ;
     uni_arrow = "->" ;
     rev_arrow = "<-" ;
     bi_arrow = "<->" ;
     uni_arrow_nopoly = "-!->" ;
-    dotnet_site_sep = ","  ;
-    dotnet_agent_sep_comma = ","  ;
-    dotnet_agent_sep_dot = "."  ;
-    sotnet_agent_set_plus = "+"
+    breakable = true ;
   }
 
-let symbol_table_V3_light =
-  {symbol_table_V3 with site_sep_comma=" "}
+let lighten symbol_table =
+  {symbol_table with site_sep = " "}
+let to_dotnet symbol_table =
+  {symbol_table with agent_sep_plus = "+" ; agent_sep_dot = "."}
 
 let symbol_table_V4 =
   {
@@ -73,23 +87,39 @@ let symbol_table_V4 =
     internal_state_symbol = "";
     open_internal_state = "{";
     close_internal_state = "}";
+    open_internal_state_mod = "";
+    close_internal_state_mod = "";
+    internal_state_mod_symbol = "/";
+    open_binding_state_mod = "";
+    close_binding_state_mod = "";
+    binding_state_mod_symbol = "";
     free = ".";
     at = "." ;
     agent_open = "(" ;
     agent_close =  ")" ;
     agent_sep_comma = "," ;
-    agent_sep_plus = "+" ;
-    agent_sep_dot = "." ;
+    agent_sep_plus = "," ;
+    agent_sep_dot = "," ;
+    compact_agent_sep_comma = false ;
+    compact_agent_sep_plus = false ;
+    compact_agent_sep_dot = false ;
     btype_sep = ".";
-    site_sep_comma = "," ;
+    site_sep = "," ;
     ghost_agent = "." ;
     show_ghost = true ;
     uni_arrow = "->" ;
     rev_arrow = "<-" ;
     bi_arrow = "<->" ;
     uni_arrow_nopoly = "-!->" ;
-    dotnet_site_sep = ","  ;
-    dotnet_agent_sep_comma = ","  ;
-    dotnet_agent_sep_dot = "."  ;
-    sotnet_agent_set_plus = "+"  
+    breakable = true ;
   }
+
+let not_breakable symbol_table = {symbol_table with breakable = false}
+
+let symbol_table_V3_light = lighten symbol_table_V3
+let symbol_table_dotnet = to_dotnet symbol_table_V3
+let unbreakable_symbol_table_V3 = not_breakable symbol_table_V3
+let unbreakable_symbol_table_V4 = not_breakable symbol_table_V4
+
+let unbreakable_symbol_table_V3_light = not_breakable symbol_table_V3_light
+let unbreakable_symbol_table_dotnet = not_breakable symbol_table_dotnet

KaSa_rep/config/symbol_table.mli
@@ -1,34 +1,45 @@
 type symbol_table =
   {
-    bound : string;
-    open_binding_state : string;
-    close_binding_state : string;
-    link_to_any : string;
-    link_to_some : string;
-    internal_state_symbol : string;
-    free : string;
-    open_internal_state : string;
-    close_internal_state : string;
-    at : string ;
     agent_open : string ;
     agent_close : string ;
-    site_sep_comma : string ;
-    btype_sep : string ;
     agent_sep_comma : string ;
     agent_sep_dot : string ;
     agent_sep_plus : string ;
+    compact_agent_sep_comma: bool ;
+    compact_agent_sep_dot: bool ;
+    compact_agent_sep_plus: bool ;
     ghost_agent : string ;
     show_ghost : bool ;
+    internal_state_symbol : string;
+    open_internal_state : string;
+    close_internal_state : string;
+    open_internal_state_mod : string;
+    close_internal_state_mod : string;
+    internal_state_mod_symbol: string;
+    open_binding_state : string;
+    close_binding_state : string;
+    open_binding_state_mod: string;
+    binding_state_mod_symbol: string;
+    close_binding_state_mod: string;
+    free : string;
+    bound : string;
+    link_to_any : string;
+    link_to_some : string;
+    at : string ;
+    site_sep : string ;
+    btype_sep : string ;
     uni_arrow : string ;
     rev_arrow : string ;
     bi_arrow : string ;
     uni_arrow_nopoly : string ;
-    dotnet_site_sep : string ;
-    dotnet_agent_sep_comma : string ;
-    dotnet_agent_sep_dot : string ;
-    sotnet_agent_set_plus : string 
+    breakable: bool ;
   }
 
 val symbol_table_V4: symbol_table
 val symbol_table_V3: symbol_table
 val symbol_table_V3_light: symbol_table
+val symbol_table_dotnet: symbol_table
+val unbreakable_symbol_table_V4: symbol_table
+val unbreakable_symbol_table_V3: symbol_table
+val unbreakable_symbol_table_V3_light: symbol_table
+val unbreakable_symbol_table_dotnet: symbol_table

odes/KaDE.ml
@@ -1,6 +1,6 @@
 (** Network/ODE generation
   * Creation: 22/07/2016
-  * Last modification: Time-stamp: <Dec 14 2017>
+  * Last modification: Time-stamp: <Dec 15 2017>
 *)
 
 module A = Odes.Make (Ode_interface)
@@ -238,6 +238,7 @@ let main ?called_from:(called_from=Remanent_parameters_sig.Server) () =
         in
         let compil =
           A.get_compil
+            ~dotnet
             ~reaction_rate_convention ~rule_rate_convention
             ~show_reactions ~count ~compute_jacobian
             cli_args preprocessed_ast
@@ -262,7 +263,7 @@ let main ?called_from:(called_from=Remanent_parameters_sig.Server) () =
             in
             let () = Format.printf "+ restart compilation to account for ~-equivalent patterns in algebraic expressions... @." in
             let compil =
-              A.get_compil ?bwd_bisim
+              A.get_compil ~dotnet ?bwd_bisim
                 ~reaction_rate_convention ~rule_rate_convention ~show_reactions ~count ~compute_jacobian
                 cli_args preprocessed_ast
             in
@@ -290,6 +291,7 @@ let main ?called_from:(called_from=Remanent_parameters_sig.Server) () =
       else
         let compil =
           A.get_compil
+            ~dotnet
             ~reaction_rate_convention ~rule_rate_convention
             ~show_reactions ~count ~compute_jacobian
             cli_args preprocessed_ast
@@ -302,7 +304,7 @@ let main ?called_from:(called_from=Remanent_parameters_sig.Server) () =
     in
     let network =
       A.network_from_compil
-        ?max_size ~dotnet ~smash_reactions ~ignore_obs parameters compil network
+        ?max_size ~smash_reactions ~ignore_obs parameters compil network
     in
     (*************************************************************)
     let out_channel =

odes/kade_backend.ml
@@ -20,7 +20,6 @@ struct
       symbol_table.Symbol_table.close_internal_state
 
   let print_cc
-      ?dotnet:(dotnet=false)
       ?full_species:(full_species=false) ?sigs ?cc_id ~with_id ?symbol_table:(symbol_table=Symbol_table.symbol_table_V4) f cc =
         let print_intf (ag_i, _ as ag) link_ids neigh =
           snd
@@ -32,9 +31,8 @@ struct
                         f "%t%a%a"
                         (if not_empty then
                            (fun fmt -> Format.fprintf fmt "%s"
-                               (if dotnet then
-                                  symbol_table.Symbol_table.dotnet_site_sep
-                                else symbol_table.Symbol_table.site_sep_comma))
+                               symbol_table.Symbol_table.site_sep
+                              )
                          else Pp.empty)
                         (Agent.print_site ?sigs ag) p
                         (print_internal_state symbol_table ?sigs ag p)
@@ -43,9 +41,8 @@ struct
                       Format.fprintf
                         f "%t%a"
                         (if not_empty then
-                          (fun fmt -> Format.fprintf f "%s"
-                            (if dotnet then symbol_table.Symbol_table.dotnet_site_sep
-                             else symbol_table.Symbol_table.site_sep_comma))
+                           (fun fmt ->
+                              Format.fprintf fmt "%s"                              symbol_table.Symbol_table.site_sep)
                          else Pp.empty)
                         (Agent.print_site ?sigs ag) p in
                   match el with
@@ -101,17 +98,20 @@ struct
                      f "%t@[<h>%a%s"
                      (if not_empty
                       then
-                        begin
-                          if dotnet then
-                            (fun fmt -> Format.fprintf fmt ".")
-                          else Pp.comma
-                        end
+                        (fun fmt ->
+                           let () =
+                             Format.fprintf fmt "%s" symbol_table.Symbol_table.agent_sep_dot
+                           in
+                           if not symbol_table.Symbol_table.compact_agent_sep_dot then
+                             Format.fprintf fmt "@ ")
                       else Pp.empty)
                      (Agent.print ?sigs ~with_id) ag_x
                      symbol_table.Symbol_table.agent_open
                  in
                  let out = print_intf ag_x link_ids el in
-                 let () = Format.fprintf f "%s@]" symbol_table.Symbol_table.agent_close in
+                 let () =
+                   Format.fprintf f "%s@]" symbol_table.Symbol_table.agent_close
+                 in
                  true, out
                else not_empty,link_ids)
             cc (false, (1, Mods.Int2Map.empty))
@@ -321,7 +321,15 @@ let print_rule_mixture
         && not !Parameter.debugModeOn
         then aux_print some t
         else
-          let () = if some then Pp.comma f in
+          let () =
+            if some then
+              let () =
+                Format.fprintf f "%s" symbol_table.Symbol_table.agent_sep_comma
+              in
+              if not symbol_table.Symbol_table.compact_agent_sep_comma
+              then
+                Format.fprintf f "@ "
+          in
           let () = print_rule_agent sigs ~ltypes ~symbol_table
               incr_agents created_incr f h in
           aux_print true t in
@@ -428,7 +436,12 @@ struct
         else
           let () =
             if some then
-              Format.pp_print_string f symbol_table.Symbol_table.agent_sep_comma
+              let () =
+                Format.fprintf f "%s" symbol_table.Symbol_table.agent_sep_comma
+              in
+              if not symbol_table.Symbol_table.compact_agent_sep_comma
+                then
+                  Format.fprintf f "@ "
           in
           let () = print_agent created true ?sigs ~symbol_table incr_agents f h
         in
@@ -493,7 +506,8 @@ struct
       Format.fprintf f "%a %a" pr_alg va (Model.print_token ~env) tok in
     Format.fprintf f "%a%t%a%t%a%t"
       (LKappa.print_rule_mixture sigs ~ltypes:false r_created) r_mix
-      (if r_mix <> [] && r_created <> [] then Pp.comma else Pp.empty)
+      (if r_mix <> [] && r_created <> [] then
+         Pp.comma else Pp.empty)
       (Raw_mixture.print ~created:true ~sigs ~symbol_table) r_created
 
       (if r.Primitives.delta_tokens <> []

odes/kade_backend.mli
@@ -1,7 +1,7 @@
 module Pattern:
 sig
   val print_cc:
-  ?dotnet:bool -> ?full_species:bool ->
+  ?full_species:bool ->
   ?sigs:Signature.s -> ?cc_id:Pattern.id -> with_id:bool ->
   ?symbol_table:Symbol_table.symbol_table ->
   Format.formatter -> Pattern.cc -> unit

odes/ode_interface.ml
@@ -22,10 +22,17 @@ type compil =
     show_reactions: bool ;
     count: Ode_args.count ;
     compute_jacobian: bool ;
-    symbol_table: Symbol_table.symbol_table
+    symbol_table: Symbol_table.symbol_table;
+    allow_empty_lhs: bool;
   }
 
+let do_we_allow_empty_lhs compil = compil.allow_empty_lhs
 
+let to_dotnet compil =
+  {compil with symbol_table = Symbol_table.unbreakable_symbol_table_dotnet}
+
+let dont_allow_empty_lhs compil =
+  {compil with allow_empty_lhs = false}
 
 type cc_cache = Pattern.PreEnv.t
 
@@ -167,10 +174,9 @@ let do_we_count_in_embeddings compil =
 let do_we_prompt_reactions compil =
   compil.show_reactions
 
-let print_chemical_species ?dotnet ?compil f =
+let print_chemical_species ?compil f =
   Format.fprintf f "@[<h>%a@]"
     (Kade_backend.Pattern.print_cc
-       ?dotnet
        ?full_species:(Some true)
        ?sigs:(Option_util.map Model.signatures (environment_opt compil))
        ?cc_id:None
@@ -587,6 +593,7 @@ let to_ast x = x
 
 let preprocess cli_args ast = Cli_init.preprocess cli_args ast
 let get_compil
+    ~dotnet
     ?bwd_bisim
     ~rule_rate_convention ?reaction_rate_convention
     ~show_reactions ~count ~compute_jacobian cli_args preprocessed_ast =
@@ -594,7 +601,8 @@ let get_compil
     Cli_init.get_compilation_from_preprocessed_ast
       ?bwd_bisim cli_args preprocessed_ast
   in
-  {
+  let compil =
+    {
     environment = env ;
     contact_map = contact_map ;
     init = init ;
@@ -603,12 +611,17 @@ let get_compil
     show_reactions = show_reactions ;
     count = count ;
     compute_jacobian = compute_jacobian ;
+    allow_empty_lhs = true ;
     symbol_table =
       match cli_args.Run_cli_args.syntaxVersion with
-      | Ast.V3 -> Symbol_table.symbol_table_V3
-      | Ast.V4 -> Symbol_table.symbol_table_V4
-
+      | Ast.V3 -> Symbol_table.unbreakable_symbol_table_V3
+      | Ast.V4 -> Symbol_table.unbreakable_symbol_table_V4 ;
   }
+  in
+  if dotnet then
+    to_dotnet (dont_allow_empty_lhs compil)
+  else
+    compil
 
 let empty_cc_cache compil =
   Pattern.PreEnv.of_env (Model.domain compil.environment)

commit febb8fa1df77ccd25c3d4274fb6a2b6e6705493f

fix to a bug introduced in commit 489c6ce6491e93aa8849445f1d2030215fd53e53, in DOTNET, we do NOT allow empty lhs

odes/odes.ml
@@ -1,6 +1,6 @@
 (** Network/ODE generation
   * Creation: 15/07/2016
-  * Last modification: Time-stamp: <Dec 15 2017>
+  * Last modification: Time-stamp: <Dec 16 2017>
 *)
 
 let local_trace = false
@@ -782,7 +782,7 @@ struct
            ([], network)
            initial_states) rules
     in
-    if I.do_we_allow_empty_lhs compil
+    if not (I.do_we_allow_empty_lhs compil)
     then
       match network.has_empty_lhs with
       | None ->