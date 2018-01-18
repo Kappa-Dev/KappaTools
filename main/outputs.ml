67e589e9dabd7fc72fd65be0b5129be026372d1d
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2018-01-18 09:52:13 +0100

    Fix #485: use correct comment sign in inputs.ka

--- main/outputs.ml
+++ main/outputs.ml
@@ -207,7 +207,7 @@ let close ?event () =
 let initial_inputs conf env contact_map init ~filename =
   let inputs = Kappa_files.open_out_fresh filename "" ".ka" in
   let inputs_form = Format.formatter_of_out_channel inputs in
-  let () = Format.fprintf inputs_form "# \"uuid\" : \"%i\"@." uuid in
+  let () = Format.fprintf inputs_form "// \"uuid\" : \"%i\"@." uuid in
   let () = Format.fprintf inputs_form
       "%a@.%a@." Configuration.print conf
       (Kappa_printer.env_kappa contact_map) env in
