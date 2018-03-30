e9e6e41670818fa723cca82df12a7088548edcf8
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2018-03-30 14:57:12 +0200

    version number for release

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def readme():
 
 setup(name='kappy',
       license='LGPLv3',
-      version='4.0.0rc1',
+      version='4.0.0',
       description='Wrapper to interact with the Kappa tool suite',
       long_description=readme(),
       url='https://github.com/Kappa-Dev/KaSim.git',
