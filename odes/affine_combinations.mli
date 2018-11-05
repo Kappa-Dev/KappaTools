260c334bf5c646b172b7494c61bff0ff30e54655
feret <feret@ens.fr>
2018-11-05 05:47:37 +0100

    remove a useless file

--- odes/affine_combinations.mli
+++
@@ -1,8 +0,0 @@
-type aff_combination
-
-val sum: aff_combination -> aff_combination -> aff_combination
-val mul_scal: int -> aff_combination -> aff_combination
-val div_scal: aff_combination -> int -> aff_combination option
-val linearise: (Pattern.id array list, int) Alg_expr.e Locality.annot -> aff_combination
-val of_int: int -> aff_combination
-val necessarily_equal: aff_combination -> aff_combination -> bool
