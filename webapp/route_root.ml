b64e489428e05e01b4e42c3b106bd562079f74e0
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2019-01-14 12:22:47 +0100

    all tools built via 'dune build'

--- webapp/route_root.ml
+++ webapp/route_root.ml
@@ -644,7 +644,8 @@ let route
           | `OPTIONS -> Webapp_common.options_respond methods
           | _ -> Webapp_common.method_not_allowed_respond methods
     };
-    (** Static analyses *)
+
+    (* Static analyses *)
     { Webapp_common.path = "/v2/projects/{projectid}/analyses" ;
       Webapp_common.operation =
         let methods = [ `OPTIONS ; `PUT ; ] in
--- webapp/server_stdio.mli
+++
@@ -1,9 +0,0 @@
-(******************************************************************************)
-(*  _  __ * The Kappa Language                                                *)
-(* | |/ / * Copyright 2010-2017 CNRS - Harvard Medical School - INRIA - IRIF  *)
-(* | ' /  *********************************************************************)
-(* | . \  * This file is distributed under the terms of the                   *)
-(* |_|\_\ * GNU Lesser General Public License Version 3                       *)
-(******************************************************************************)
-
-val serve : ?buffer:string -> unit -> unit Lwt.t
