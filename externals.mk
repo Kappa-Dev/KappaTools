74d26853cefca3f6f22f2ce68f40a93f20e87385
Yvan Sraka <yvan@sraka.pw>
2019-03-12 14:04:32 +0100

    Update packaging Makefile rules using Electron.js instead of NWJS
    https://electronjs.org/
    
    This experiment was firstly motivated by finding a workaround to deal
    with the "blur" bug [1] (on Apple retina displays).
    
    I discover by comparing Electron default Info.plist and the one
    generated by Info.plist.skel that we lack the property:
    
    	<key>NSHighResolutionCapable</key>
    	<true/>
    
    as explained in this old documentation:
    https://developer.apple.com/library/archive/documentation/GraphicsAnimation/Conceptual/HighResolutionOSX/Explained/Explained.html
    
    The choice of switching from NWJS to Electron was motivated by the:
    - good support of Electron by community and Microsoft
    - native capabilities API we let us implement in a near futur:
    	* automatic updates (eg. from GitHub releases)
    	* crash reports (eg. to GitHub issues)

--- externals.mk
+++ externals.mk
@@ -1,5 +1,7 @@
-NWJS_VERSION:=0.35.5
 CODEMIRROR_VERSION:=5.42.2
 BOOTSTRAP_VERSION:=3.3.7
 JQUERY_VERSION:=3.2.1
 JQUERY_UI_VERSION:=1.12.1
+
+# https://github.com/electron/electron-api-demos/releases
+ELECTRON_VERSION:=3.1.6
