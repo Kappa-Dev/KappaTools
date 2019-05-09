4703b73c5c610eae8a854f073d58bc448e228772
Yarden Katz <yarden.katz@gmail.com>
2019-05-09 09:06:08 -0400

    make build fail gracefully

--- setup.py
+++ setup.py
@@ -6,6 +6,16 @@ import subprocess
 import setuptools.command.build_ext
 import os.path
 
+def should_build_agents():
+    """
+    Should we build agents? Currently this is decided based on
+    checking for existence of a .ml file. Ideally, this should
+    be made into a flag.
+    """
+    if os.path.isfile('agents/KaMoHa.ml'):
+        return True
+    return False
+
 class BuildAgentsCommand(distutils.cmd.Command):
     """Instruction to compile Kappa agents"""
 
@@ -21,25 +31,36 @@ class BuildAgentsCommand(distutils.cmd.Command):
         ()
 
     def run(self):
-        if os.path.isfile('agents/KaMoHa.ml'):
-            subprocess.check_call(["make","all","agents"])
+        status = False
+        if should_build_agents():
+            try:
+                subprocess.check_call(["make","all","agents"])
+            except subprocess.CalledProcessError:
+                print "Failed to compile Kappa agents. Installing Python " \
+                      "wrapper only."
+                return status
+            status = True
+        return status
 
 class MyBuildExtCommand(setuptools.command.build_ext.build_ext):
     """Compile Kappa agent in addition of standard build"""
 
     def run(self):
         self.my_outputs = []
-        if os.path.isfile('agents/KaMoHa.ml'):
-            self.run_command('build_agents')
-            bin_dir = os.path.join(self.build_lib, 'kappy/bin')
-            distutils.dir_util.mkpath(bin_dir)
-            distutils.file_util.copy_file("bin/KaSimAgent", bin_dir)
-            self.my_outputs.append(os.path.join(bin_dir, "KaSimAgent"))
-            distutils.file_util.copy_file("bin/KaSaAgent", bin_dir)
-            self.my_outputs.append(os.path.join(bin_dir, "KaSaAgent"))
-            distutils.file_util.copy_file("bin/KaMoHa", bin_dir)
-            self.my_outputs.append(os.path.join(bin_dir, "KaMoHa"))
-            setuptools.command.build_ext.build_ext.run(self)
+        if should_build_agents():
+            build_success = self.run_command('build_agents')
+            if build_success:
+                # if we were able to build agents, move them to
+                # appropriate binary directories
+                bin_dir = os.path.join(self.build_lib, 'kappy/bin')
+                distutils.dir_util.mkpath(bin_dir)
+                distutils.file_util.copy_file("bin/KaSimAgent", bin_dir)
+                self.my_outputs.append(os.path.join(bin_dir, "KaSimAgent"))
+                distutils.file_util.copy_file("bin/KaSaAgent", bin_dir)
+                self.my_outputs.append(os.path.join(bin_dir, "KaSaAgent"))
+                distutils.file_util.copy_file("bin/KaMoHa", bin_dir)
+                self.my_outputs.append(os.path.join(bin_dir, "KaMoHa"))
+                setuptools.command.build_ext.build_ext.run(self)
 
     def get_outputs(self):
         outputs = setuptools.command.build_ext.build_ext.get_outputs(self)
