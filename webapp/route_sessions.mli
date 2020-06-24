241fd564ca28f55a82612e7fc6b8df888b09f414
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2020-06-24 16:31:01 +0200

    Cleanup induced by dune 2

--- webapp/route_sessions.mli
+++
@@ -1,9 +0,0 @@
-(******************************************************************************)
-(*  _  __ * The Kappa Language                                                *)
-(* | |/ / * Copyright 2010-2020 CNRS - Harvard Medical School - INRIA - IRIF  *)
-(* | ' /  *********************************************************************)
-(* | . \  * This file is distributed under the terms of the                   *)
-(* |_|\_\ * GNU Lesser General Public License Version 3                       *)
-(******************************************************************************)
-
-val route : manager:Api.manager -> Webapp_common.route_handler list
