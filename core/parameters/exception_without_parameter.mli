21bfd93181e931103fe1ee488ecc7dc30f18053d
Antoine Pouille <m.pouille.antoine+github@gmail.com>
2024-09-06 12:04:15 +0200

    Add doc to exception_without_parameter.mli

--- core/parameters/exception_without_parameter.mli
+++ core/parameters/exception_without_parameter.mli
@@ -12,6 +12,13 @@
     * en Automatique.  All rights reserved.  This file is distributed
     *  under the terms of the GNU Library General Public License *)
 
+(* The logic in this files describes a `degraded` mode for exceptions, before
+   the remanent_parameters were loaded.
+   these exceptions did not stop execution, so `uncaught` and `caught` may not
+   be good naming here. `caught` here basically add a trace to uncaught info
+   TODO: revamp/rename this?
+   *)
+
 type uncaught_exception
 
 exception Uncaught_exception of uncaught_exception
@@ -31,7 +38,10 @@ val build_caught_exception :
   string option -> string option -> exn -> string list -> caught_exception
 
 val add_uncaught_error :
-  ?to_ui:bool -> uncaught_exception -> exceptions_caught_and_uncaught -> exceptions_caught_and_uncaught
+  ?to_ui:bool ->
+  uncaught_exception ->
+  exceptions_caught_and_uncaught ->
+  exceptions_caught_and_uncaught
 
 val stringlist_of_exception : exn -> string list -> string list
 val stringlist_of_uncaught : uncaught_exception -> string list -> string list
@@ -41,10 +51,18 @@ val pp_exception : Format.formatter -> exn -> unit
 val pp_uncaught : Format.formatter -> uncaught_exception -> unit
 val pp_caught : Format.formatter -> caught_exception -> unit
 val empty_exceptions_caught_and_uncaught : exceptions_caught_and_uncaught
-val is_empty_exceptions_caught_and_uncaught : exceptions_caught_and_uncaught -> bool
-val get_caught_exception_list : exceptions_caught_and_uncaught -> caught_exception list
-val get_caught_exception_list_to_ui : exceptions_caught_and_uncaught -> caught_exception list
-val get_uncaught_exception_list : exceptions_caught_and_uncaught -> uncaught_exception list
+
+val is_empty_exceptions_caught_and_uncaught :
+  exceptions_caught_and_uncaught -> bool
+
+val get_caught_exception_list :
+  exceptions_caught_and_uncaught -> caught_exception list
+
+val get_caught_exception_list_to_ui :
+  exceptions_caught_and_uncaught -> caught_exception list
+
+val get_uncaught_exception_list :
+  exceptions_caught_and_uncaught -> uncaught_exception list
 
 val get_uncaught_exception_list_to_ui :
   exceptions_caught_and_uncaught -> uncaught_exception list
