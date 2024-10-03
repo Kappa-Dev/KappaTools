b2c9e810d30279edb21f582e1b83606e7e5d132e
Antoine Pouille <m.pouille.antoine+github@gmail.com>
2024-10-03 16:40:27 +0200

    Untested tests written

--- tests/playwright/procedure.spec.ts
+++ tests/playwright/procedure.spec.ts
@@ -8,7 +8,7 @@ import * as utils from './webapp_utils';
 test.describe.configure({ mode: 'parallel' });
 
 test.beforeEach(async ({ page }, testInfo) => {
-  page; // TODO: remove
+  page; // TODO: remove when `page` used in function
   testInfo.setTimeout(testInfo.timeout + 30000);
 });
 
@@ -18,6 +18,7 @@ const poly_ka = '//raw.githubusercontent.com/Kappa-Dev/KappaTools/master/example
 const local_views_slide_69_ka = '//www.di.ens.fr/~feret/teaching/2023-2024/MPRI.2.19/activities/local_views/local_views_slide_69.ka'
 const counter_2_ka = '//raw.githubusercontent.com/Kappa-Dev/KappaTools/master/tests/integration/compiler/counters_2_levels/counter_2.ka'
 const minikai_counters_ka = '//raw.githubusercontent.com/Kappa-Dev/KappaTools/master/examples/large/minikai/minikai_counters.ka'
+const causality_slide_10_ka = '//www.di.ens.fr/~feret/teaching/2023-2024/MPRI.2.19/activities/causality/causality_slide_10.ka'
 
 test.describe('Editor tab', () => {
 
@@ -31,40 +32,36 @@ test.describe('Editor tab', () => {
     async function editor_cancel(): Promise<void> {
       await editor.press('ControlOrMeta+z');
     }
-    const error_field = utils.get_error_field(page);
-    async function expect_error(text: string[]): Promise<void> {
-      await expect.soft(error_field).toHaveText(text);
-    }
 
     await page.locator('div:nth-child(21) > .CodeMirror-line').click();
     await editor_to_line(21);
     // Make a syntax error
     await editor.press('Backspace');
     // (useless comment to match brackets { {)
-    await expect_error([
+    await utils.expect_error(page, [
       " « 1/1 » [abc.ka] invalid internal state or missing '}' ",
       " invalid internal state or missing '}' ",
     ]);
     await editor_cancel();
-    await expect_error([
+    await utils.expect_error(page, [
       " «  » ",
       "",
     ]);
     await editor_to_line(24);
     await editor.fill('\n%agent: D(a{u p})');
-    await expect_error([
+    await utils.expect_error(page, [
       " « 1/1 » [abc.ka] Dead agent D ",
       " Dead agent D ",
     ]);
     await editor_to_line(25);
     await editor.fill("\n'd' D(a{p}) -> D(a{u}) @ 1");
-    await expect_error([
+    await utils.expect_error(page, [
       " « 1/4 » [abc.ka] Dead agent D ",
       " Dead agent D ",
     ]);
     await editor_cancel();
     await editor_cancel();
-    await expect_error([
+    await utils.expect_error(page, [
       " «  » ",
       "",
     ]);
@@ -158,7 +155,7 @@ C(x2)  =>  [ C(x2{u}) v C(x2{p}) ]
   });
 
   test('constraints_and_polymers_3', async ({ page }) => {
-    await utils.open_app_with_model(page, local_views_slide_69_ka);
+    await utils.open_app_with_model(page, local_views_slide_69_ka, true);
     await page.getByRole('tab', { name: 'constraints' }).click();
     await expect.soft(page.locator('.panel-scroll > div > .panel-body').first()).toHaveText(
       `A(c)  =>  [ A(c[.]) v A(c[x1.C]) v A(c[x2.C]) ]
@@ -186,6 +183,10 @@ C(x2)  =>  [ C(x2{u}) v C(x2{p}) ]
       "C()  =>  [ C(x1{p}[.],x2{u}[.]) v C(x1{p}[.],x2{u}[c.A]) v C(x1{p}[.],x2{p}[.]) v C(x1{u}[c.A],x2{u}[.]) v C(x1{u}[.],x2{u}[.]) ]"
 
     );
+    await utils.expect_error(page, [
+      " « 1/4 » [abc.ka] Dead agent D ",
+      " Dead agent D ",
+    ]);
   });
 
   test('constraints_and_polymers_4', async ({ page }) => {
@@ -210,21 +211,17 @@ C(x2)  =>  [ C(x2{u}) v C(x2{p}) ]
 test.describe('Simulation tools', () => {
 
   test('Simulation, plot', async ({ page }) => {
-    async function wait_for_sim_stop(page: Page) {
-      await page.waitForTimeout(200);
-      await expect(page.getByRole('button', { name: 'intervention' })).toBeVisible({ timeout: 20000 });
-    }
     await utils.open_app_with_model(page, abc_ka);
     await utils.setSeed(page, 1);
     // Run simulation to 30, then 100, then test plot options
     await utils.set_pause_if(page, '[T] > 30');
     await page.getByRole('button', { name: 'start' }).click();
-    await wait_for_sim_stop(page);
+    await utils.wait_for_sim_stop(page, { timeout: 20000 });
     await page.getByRole('tab', { name: 'plot New' }).click();
     await expect.soft(page.getByRole('img')).toHaveScreenshot();
     await utils.set_pause_if(page, '[T] > 100');
     await page.getByRole('button', { name: 'continue' }).click();
-    await wait_for_sim_stop(page);
+    await utils.wait_for_sim_stop(page, { timeout: 20000 });
     await expect.soft(page.getByRole('img')).toHaveScreenshot();
     await page.getByRole('checkbox', { name: 'Log X' }).check();
     await expect.soft(page.getByRole('img')).toHaveScreenshot();
@@ -254,7 +251,7 @@ test.describe('Simulation tools', () => {
     // Test larger plots, slider
     await utils.set_pause_if(page, '[T] > 2000');
     await page.getByRole('button', { name: 'continue' }).click();
-    await wait_for_sim_stop(page);
+    await utils.wait_for_sim_stop(page, { timeout: 20000 });
     await expect.soft(page.getByRole('img')).toHaveScreenshot();
     await page.getByRole('button', { name: 'continue' }).click();
     await page.getByPlaceholder('offset').fill('0');
@@ -360,61 +357,6 @@ test.describe('Simulation tools', () => {
     // Generate two snapshots
     await utils.set_pause_if(page, '[T] > 30');
     await page.getByRole('button', { name: 'start' }).click();
-    await utils.apply_perturbation(page, '');
-    await expect(utils.get_error_field(page)).toHaveText("TODO");
-    await utils.apply_perturbation(page, '$SNAPSHOT');
-    await utils.set_pause_if(page, '[T] > 60');
-    await page.getByRole('button', { name: 'continue' }).click();
-    await utils.apply_perturbation(page, '$SNAPSHOT "T60"');
-
-    // check log page
-    await page.getByRole('tab', { name: 'log New' }).click();
-    await expect(page.locator('#log div')).toHaveText("TODO");
-
-    // go to tab snapshots and test display
-    await page.getByRole('tab', { name: 'snapshot' }).click();
-    const snapshot_display_loc = page.locator('.navcontent-view > .panel-scroll').first();
-    //const snapshot_map_display_loc = page.locator('#snapshot-map-display #map-container').first();
-    //const snapshot_map_display2_loc = page.locator('#snapshot-map-display div').first();
-
-    await expect(snapshot_display_loc).toHaveText("TODO");
-    await page.locator('#snapshot-select-id').selectOption('1');
-    await expect(snapshot_display_loc).toHaveText("TODO");
-    await page.locator('#snapshot-select-id').selectOption('0');
-    await expect(snapshot_display_loc).toHaveText("TODO");
-    await page.locator('#format_select_id').selectOption('Graph');
-    await expect(snapshot_display_loc).toHaveScreenshot();
-    await page.locator('.navcontent-view > div:nth-child(3)').click();
-    await expect(snapshot_display_loc).toHaveScreenshot();
-    await page.getByRole('button', { name: 'Back to root' }).click();
-    await expect(snapshot_display_loc).toHaveScreenshot();
-    await page.getByRole('radio', { name: 'Count' }).check();
-    await expect(snapshot_display_loc).toHaveScreenshot();
-    await page.getByRole('radio', { name: 'Size' }).check();
-    await expect(snapshot_display_loc).toHaveScreenshot();
-    // TODO: does something?
-    // await page.getByRole('button', { name: 'Reset Zoom' }).click();
-
-    // TODO: clicks in graph, link with contact map
-    await page.locator('[id="root\\.mixture1"] rect').nth(1).click();
-    await expect(snapshot_display_loc).toHaveScreenshot();
-    // await page.locator('#force-container circle').nth(1).click();
-    // await page.locator('#force-container circle').first().click();
-
-    // Test exports
-    await page.locator('#format_select_id').selectOption('Kappa');
-    await utils.testExports(page, "#export_snapshot_kappa", "snapshot_kappa", ["json", "kappa", "dot"]);
-    await page.locator('#format_select_id').selectOption('Graph');
-    await utils.testExports(page, "#export_snapshot_graph", "snapshot_graph", ["json", "kappa", "dot", "svg", "png"]);
-  });
-
-  test('outputs', async ({ page }) => {
-    await utils.open_app_with_model(page, abc_ka);
-    await utils.setSeed(page, 1);
-
-    // Generate two snapshots
-    await utils.set_pause_if(page, '[T] > 30');
-    await page.getByRole('button', { name: 'start' }).click();
     await utils.apply_perturbation(page, '$PRINT "time: ".[T] > "time.txt"');
     await utils.apply_perturbation(page, '$PRINT \'AB\' > "ab.txt"');
     await utils.set_pause_if(page, '[T] > 60');
@@ -442,7 +384,149 @@ test.describe('Simulation tools', () => {
 });
 
 test.describe('stories', () => {
+
+  test('trace', async ({ page }) => {
+    await utils.open_app_with_model(page, causality_slide_10_ka, true);
+    await utils.setSeed(page, 1);
+
+    // Enable trace
+    await page.getByRole('list').locator('a').nth(2).click();
+    await page.locator('.col-md-offset-2 > label').first().click();
+    await page.getByRole('button', { name: 'Set', exact: true }).click();
+    // gather trace
+    await page.getByRole('button', { name: 'start' }).click();
+    await utils.wait_for_sim_stop(page, { timeout: 20000 });
+
+    async function expect_texts_story_logs(page: Page, text_info_log: string, text_computation_log: string) {
+      await page.getByRole('tab', { name: 'story_info_log' }).click();
+      await expect.soft(page.locator('#story_info_log')).toHaveText(text_info_log);
+      await page.getByRole('tab', { name: 'stories_computation_log' }).click();
+      await expect.soft(page.locator('#story_computation_log')).toHaveText(text_computation_log);
+      await page.getByRole('tab', { name: 'story_info_log' }).click();
+    }
+
+    async function computeStoriesAndWait(page: Page) {
+      await page.getByRole('button', { name: 'Launch' }).click();
+      // wait for icon in project to be checkmark
+      await utils.wait_for_project_ready_status(page, { timeout: 30000 });
+    }
+
+    async function computeStoriesAndTest(page: Page, text_info_log: string, text_computation_log: string) {
+      await computeStoriesAndWait(page);
+      await expect_texts_story_logs(page, text_info_log, text_computation_log);
+      await expect(page.getByRole('img')).toHaveScreenshot();
+    }
+
+    // compare stories
+    await page.getByRole('tab', { name: 'stories' }).click();
+
+    await page.getByRole('checkbox', { name: 'Weakly' }).uncheck();
+    await computeStoriesAndTest(page, "TODO", "TODO");
+
+    await page.getByRole('checkbox', { name: 'Strongly' }).check();
+    await page.getByRole('checkbox', { name: 'Strongly' }).uncheck();
+    await computeStoriesAndTest(page, "TODO", "TODO");
+    await page.getByRole('checkbox', { name: 'Causal' }).check();
+    await page.getByRole('checkbox', { name: 'Causal' }).uncheck();
+    await computeStoriesAndTest(page, "TODO", "TODO");
+
+    await page.getByRole('combobox').selectOption('0');
+    await expect(page.getByRole('img')).toHaveScreenshot();
+    await expect_texts_story_logs(page, "TODO", "TODO");
+    await page.getByRole('combobox').selectOption('2');
+    await expect(page.getByRole('img')).toHaveScreenshot();
+    await expect_texts_story_logs(page, "TODO", "TODO");
+
+    await page.getByRole('checkbox', { name: 'Weakly' }).check();
+    await page.getByRole('checkbox', { name: 'Strongly' }).check();
+    await computeStoriesAndTest(page, "TODO", "TODO");
+
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('button', { name: 'get trace' }).click();
+    const download = await downloadPromise;
+    await utils.compare_download_to_ref(download, "stories_trace");
+  });
+
 });
 
 test.describe('projects_and_files', () => {
+  test('project', async ({ page }) => {
+    await utils.open_app_with_model(page, causality_slide_10_ka, true);
+    await utils.setSeed(page, 1);
+
+    // open new project
+    await page.getByRole('list').locator('a').nth(1).click();
+    await page.getByPlaceholder('project new').click();
+    await page.getByPlaceholder('project new').fill('test');
+    await page.getByPlaceholder('project new').press('Enter');
+    // add new file "abc.ka" and fill it
+    await page.getByRole('button', { name: 'File' }).click();
+    await page.locator('#menu-editor-file-new-li').click();
+    await page.getByRole('textbox', { name: 'file name' }).click();
+    await page.getByRole('textbox', { name: 'file name' }).fill('abc.ka');
+    await page.getByRole('button', { name: 'Create File' }).click();
+    await page.locator('.CodeMirror-scroll').click();
+    await utils.input_in_editor_from_url(page, abc_ka);
+
+    // switch and check contact_map change
+    const contact_map = page.locator('#map-container');
+    await expect.soft(contact_map).toHaveScreenshot();
+    await page.getByRole('list').locator('a').nth(1).click();
+    await expect.soft(contact_map).toHaveScreenshot();
+    await page.getByRole('list').locator('a').first().click();
+    await expect.soft(contact_map).toHaveScreenshot();
+
+    // TODO: Could also check simulation results
+  });
+
+  test('files', async ({ page }) => {
+    await utils.open_app_with_model(page, abc_ka, false);
+    await utils.setSeed(page, 1);
+
+    // download file
+    await page.getByRole('button', { name: 'File' }).click();
+    const downloadPromise = page.waitForEvent('download');
+    await page.locator('#menu-editor-file-export-li').click();
+    const download = await downloadPromise;
+    await utils.compare_download_to_ref(download, "abc_download.ka");
+    const downloaded_path = await download.path();
+
+    // close it, and reopen it
+    await page.getByRole('button', { name: 'File' }).click();
+    await page.locator('#menu-editor-file-close-li').click();
+    await page.getByRole('button', { name: 'File' }).click();
+    await page.locator('#menu-editor-file-open-li').click();
+    await page.locator('body').setInputFiles(downloaded_path);
+
+    // write other file and check contact map
+    await page.getByRole('button', { name: 'File' }).click();
+    await page.locator('#menu-editor-file-new-li').click();
+    await page.getByRole('textbox', { name: 'file name' }).click();
+    await page.getByRole('textbox', { name: 'file name' }).fill('test.ka');
+    await page.getByRole('textbox', { name: 'file name' }).press('Enter');
+    await page.locator('.CodeMirror-scroll').click();
+    await utils.input_in_editor_from_str(page,
+      `%agent: K(x)
+%agent: S(a b{u p} c{u p})
+%init: 1000 K()
+%init: 1000 S()
+'K.S' K(x[.]),S(a[.]) -> K(x[1]),S(a[1]) @ 1
+'b+' K(x[1]),S(a[1] b{u}) -> K(x[1]),S(a[1] b{p}) @ 1
+'K..S' S(a[_]) -> S(a[.]) @ 10
+ 'c+' K(x[1]),S(a[1] c{u}) -> K(x[1]),S(a[1] c{p}) @ 1
+%obs: 'S++' |S(b{p} c{p})|
+%mod: [true] do $TRACK 'S++' [true] ;
+`
+    );
+    const contact_map = page.locator('#map-container');
+    await expect.soft(contact_map).toHaveScreenshot();
+
+    // simulate and test screenshot
+    await utils.set_pause_if(page, '[T] > 30');
+    await page.getByRole('button', { name: 'start' }).click();
+    await utils.wait_for_sim_stop(page, { timeout: 20000 });
+    await page.getByRole('tab', { name: 'plot New' }).click();
+    await expect.soft(page.getByRole('img')).toHaveScreenshot();
+  });
+
 });
---
+++ tests/playwright/webapp_utils.ts
@@ -0,0 +1,172 @@
+import { expect, type Page, type Download } from '@playwright/test';
+import fs from 'node:fs';
+import path from 'node:path';
+import yauzl from 'yauzl';
+
+// TODO: uniformize all functions to camlcase, take care of induced collisions
+
+// const url = 'https://tools.kappalanguage.org/try/'
+const url = 'http://127.0.0.1:12345/index.html'
+const arg_set_model = '?model=https%3A'
+
+const referencesDir = 'tests/playwright/refs/'
+
+export async function wait_for_project_ready_status(page: Page, options?: { timeout?: number | undefined; visible?: boolean | undefined; } | undefined) {
+  // wait for icon in project to be checkmark
+  await expect(page.getByRole('list').locator('a').first()).toHaveClass("glyphicon glyphicon-ok", options);
+}
+
+export async function wait_for_sim_stop(page: Page, options?: { timeout?: number | undefined; visible?: boolean | undefined; } | undefined) {
+  await page.waitForTimeout(200);
+  // TODO: simplify?
+  await expect(page.getByRole('button', { name: 'intervention' })).toBeVisible(options);
+  await wait_for_project_ready_status(page);
+}
+
+export async function text_of_url(url_protocol_relative: string) {
+  const response = await fetch('https://' + url_protocol_relative);
+  expect(response.ok).toBeTruthy();
+  return await response.text();
+}
+export async function wait_for_file_load(page: Page, options?: { timeout?: number | undefined; visible?: boolean | undefined; } | undefined) {
+  await page.waitForTimeout(200);
+  // wait for contact_map display to appear, as it's the last thing to visibly load
+  await expect(page.locator('.nodeArcPath').first()).toBeVisible(options);
+  await wait_for_project_ready_status(page);
+}
+
+export async function input_in_editor_from_str(page: Page, model: string): Promise<void> {
+  await page.locator('.CodeMirror-scroll').click();
+  await page.locator('#editor-panel').getByRole('textbox').fill(model);
+  await wait_for_file_load(page, { timeout: 10000 });
+}
+
+export async function input_in_editor_from_url(page: Page, url_protocol_relative: string): Promise<void> {
+  const model = await text_of_url(url_protocol_relative);
+  await input_in_editor_from_str(page, model);
+}
+
+export async function open_app_with_model(page: Page, url_protocol_relative: string, paste_in_editor: boolean = false) {
+  if (paste_in_editor) {
+    // download the file and paste it in the editor
+    await page.goto(url);
+    await input_in_editor_from_url(page, url_protocol_relative);
+  } else {
+    // provide the file as model argument (ok with github domains, but e.g. not di.ens.fr)
+    await page.goto(url + arg_set_model + url_protocol_relative);
+  }
+  // Note: if fails in input_in_editor_from_str, it won't wait for second timeout as expect is not expect.soft
+  await wait_for_file_load(page, { timeout: 10000 });
+}
+
+export function get_error_field(page: Page) {
+  return page.locator('#configuration_error_div');
+}
+
+export async function expect_error(page: Page, text: string[]): Promise<void> {
+  await expect.soft(get_error_field(page)).toHaveText(text);
+}
+
+export async function set_pause_if(page: Page, s: string) {
+  await page.getByPlaceholder('[T] >').click();
+  await page.getByPlaceholder('[T] >').fill(s);
+  await page.getByPlaceholder('[T] >').press('Enter');
+}
+
+export async function setSeed(page: Page, seed: number) {
+  await page.getByRole('list').locator('a').nth(2).click();
+  await page.getByRole('spinbutton', { name: 'Seed' }).click();
+  await page.getByRole('spinbutton', { name: 'Seed' }).fill(seed.toString());
+  await page.getByRole('button', { name: 'Set', exact: true }).click();
+  // wait for modal to close
+  await expect(page.locator('div:nth-child(5) > .col-md-2')).toBeHidden();
+}
+
+export async function apply_perturbation(page: Page, s: string) {
+  await page.getByPlaceholder('Simulation Perturbation').click();
+  await page.getByPlaceholder('Simulation Perturbation').fill(s);
+  await page.getByRole('button', { name: 'intervention' }).click();
+}
+
+export async function write_ref(filePath: string, fileName: string) {
+  expect.soft(filePath).toBeTruthy();
+  const refFilePath = path.join(referencesDir, fileName);
+  fs.copyFileSync(filePath, refFilePath);
+}
+
+export async function compare_download_to_ref(download: Download, fileName: string, writeRef: boolean = false) {
+  const filePath = await download.path();
+  expect.soft(filePath).toBeTruthy();
+  if (writeRef || process.env.UPDATE_EXPORTS === 'true') {
+    await write_ref(filePath, fileName);
+  }
+  const refFilePath = path.join(referencesDir, fileName);
+  const downloadedContent = await fs.promises.readFile(filePath, 'utf8');
+  const referenceContent = await fs.promises.readFile(refFilePath, 'utf8');
+  expect.soft(downloadedContent).toBe(referenceContent);
+}
+
+export async function write_str_ref(test: string, fileName: string) {
+  const filePath = path.join(referencesDir, fileName);
+  fs.writeFile(filePath, test,
+    err => {
+      if (err) {
+        console.error(`Error writing to file: ${err}`);
+      }
+    });
+}
+export async function get_ref(fileName: string) {
+  const refFilePath = path.join(referencesDir, fileName);
+  const referenceContent = await fs.promises.readFile(refFilePath, 'utf8');
+  return referenceContent;
+}
+
+export async function compare_str_to_ref(text: string, fileName: string, writeRef: boolean = false) {
+  if (writeRef || process.env.UPDATE_EXPORTS === 'true') {
+    await write_str_ref(text, fileName);
+  }
+  const refFilePath = path.join(referencesDir, fileName);
+  const referenceContent = await fs.promises.readFile(refFilePath, 'utf8');
+  expect.soft(text).toBe(referenceContent);
+}
+
+export async function testExports(page: Page, exportLocatorPrefix: string, fileBaseName: string, extensions: string[], writeRef: boolean = false) {
+  const exportFilenameLoc = page.locator(exportLocatorPrefix + "_filename");
+  const exportSelectLoc = page.locator(exportLocatorPrefix + "_select");
+
+  for (const extension of extensions) {
+    const fileName = `${fileBaseName}.${extension}`;
+    await exportSelectLoc.selectOption(extension);
+    await exportFilenameLoc.click();
+    await exportFilenameLoc.fill(fileName);
+
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('button', { name: 'export' }).click();
+    const download = await downloadPromise;
+
+    await compare_download_to_ref(download, fileName, writeRef);
+  }
+}
+
+export async function compare_zip_files_list_with_ref(download: Download, referenceFiles: string[]): Promise<void> {
+  // TODO: debug this
+  const downloadPath = await download.path();
+  expect(downloadPath).toBeTruthy();
+
+  // const extractDir = fs.mkdtemp(path.join(os.tmpdir(), 'extract-'),
+  //   (err, folder) => {
+  //     if (err) throw err;
+  //   });
+
+  const downloadedFiles: string[] = [];
+  yauzl.open(downloadPath, { lazyEntries: true }, function(err, zipfile: yauzl.ZipFile) {
+    if (err) throw err;
+    zipfile.readEntry();
+    zipfile.on("entry", function(entry: yauzl.Entry) {
+      downloadedFiles.push(entry.fileName);
+    });
+  });
+
+  expect(downloadedFiles.sort()).toEqual(referenceFiles.sort());
+}
+
