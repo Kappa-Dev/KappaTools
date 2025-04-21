0c82af918e2889945d0654b8471b1bdbbe33c67d
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2025-04-21 23:19:08 +0200

    formula simplification

--- core/KaSa_rep/abstract_domains/mvbdu/logical_formulae.ml
+++ core/KaSa_rep/abstract_domains/mvbdu/logical_formulae.ml
@@ -7,6 +7,54 @@ type 'a formula =
   | False 
   | True 
 
+let op x y = 
+  x = NOT y || y = NOT x 
+
+let rec simplify f = 
+  match f with
+  | NOT x -> 
+    let x = simplify x in 
+    begin 
+      match x with 
+        | NOT x -> x 
+        | True -> False 
+        | False -> True 
+        | IMPLY _ | OR _ | AND _ | P _ -> NOT x 
+    end
+  | IMPLY (x, y) -> simplify (OR(NOT x,y))
+  | OR (x,y) -> 
+    let x = simplify x in 
+    let y = simplify y in 
+    begin
+      match x,y with 
+      | True, _ | _, True -> True 
+      | False, x| x, False -> x 
+      | x, AND(y,z)  when op x y -> OR(x,z)
+      | AND(y,z), x  when op x y -> OR(x,z)
+      | x, AND(z,y)  when op x y -> OR(x,z)
+      | AND(z,y), x when op x y -> OR(x,z)
+    
+      | (OR _ | AND _ | NOT _ | IMPLY _ | P _),
+      (OR _ | AND _ | NOT _ | IMPLY _ | P _) -> OR (x,y) 
+    end 
+    | AND (x,y) -> 
+      let x = simplify x in 
+      let y = simplify y in 
+      begin
+        match x,y with 
+        | False, _ | _, False -> False
+        | True, x| x, True -> x 
+        | x, OR(y,z)  when op x y -> AND(x,z)
+        | OR(y,z), x  when op x y -> AND(x,z)
+        | x, OR(z,y)  when op x y -> AND(x,z)
+        | OR(z,y), x when op x y -> AND(x,z)
+        | (OR _ | AND _ | NOT _ | IMPLY _ | P _),
+        (OR _ | AND _ | NOT _ | IMPLY _ | P _) -> AND (x,y) 
+      end 
+    | True | False | P _ -> f
+
+
+
 let rec print parameter error string_of formula = 
   match formula with 
   | P x -> 
@@ -39,3 +87,4 @@ and print_arg parameter error string_of formula  =
     let error = print parameter error string_of formula in 
     let () = Loggers.fprintf (Remanent_parameters.get_logger parameter) ")" in error 
 
+    let print p e s f = print p e s (simplify f)
