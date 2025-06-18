d2b754309ac6a543ff9e1e88b813c7de11ec1927
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2025-06-18 09:23:29 +0200

    make some types more explicit

--- core/KaSa_rep/abstract_domains/mvbdu/memo_sig.ml
+++ core/KaSa_rep/abstract_domains/mvbdu/memo_sig.ml
@@ -12,61 +12,60 @@
    * en Automatique.  All rights reserved.  This file is distributed
    * under the terms of the GNU Library General Public License *)
 
-type ('bool, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'd, 'memo_tables, 'int, 'h) memoized_fun = {
+type ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'function_type, 'd, 'memo_tables, 'int, 'mvbdu) memoized_fun = {
   f:
     Remanent_parameters_sig.parameters ->
     Exception.exceptions_caught_and_uncaught ->
-    'c;
+    'function_type;
   store:
     Remanent_parameters_sig.parameters ->
-    Exception.exceptions_caught_and_uncaught ->
-     
-    ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'bool, 'int) handler ->
+    Exception.exceptions_caught_and_uncaught ->   
+    ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'mvbdu_leave_data, 'int) handler ->
     'd ->
-    'h ->
+    'mvbdu ->
     Exception.exceptions_caught_and_uncaught
-    * ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'bool, 'int) handler;
+    * ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'mvbdu_leave_data, 'int) handler;
   get:
     Remanent_parameters_sig.parameters ->
     Exception.exceptions_caught_and_uncaught ->
-    ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'bool, 'int) handler ->
+    ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'mvbdu_leave_data, 'int) handler ->
     'd ->
     Exception.exceptions_caught_and_uncaught
-    * (('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'bool, 'int) handler * 'h option);
+    * (('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'mvbdu_leave_data, 'int) handler * 'mvbdu option);
 }
 
-and ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'bool, 'int) handler = {
+and ('memo_tables, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'mvbdu_leave_data, 'int) handler = {
   data: 'memo_tables;
   mvbdu_dictionary: 'mvbdu_dic;
   association_list_dictionary: 'blist;
   variables_list_dictionary: 'vlist;
   range_list_dictionary: 'rlist;
-  print_cell: Remanent_parameters_sig.parameters -> 'bool Mvbdu_sig.cell -> unit;
+  print_cell: Remanent_parameters_sig.parameters -> 'mvbdu_leave_data Mvbdu_sig.cell -> unit;
   print_skel:
-    Remanent_parameters_sig.parameters -> 'bool Mvbdu_sig.skeleton -> unit;
-  print_mvbdu: Remanent_parameters_sig.parameters -> 'bool Mvbdu_sig.mvbdu -> unit;
+    Remanent_parameters_sig.parameters -> 'mvbdu_leave_data Mvbdu_sig.skeleton -> unit;
+  print_mvbdu: Remanent_parameters_sig.parameters -> 'mvbdu_leave_data Mvbdu_sig.mvbdu -> unit;
 }
 
 type 'a pair = 'a * 'a
 
-type ('a, 'b, 'blist, 'rlist, 'vlist, 'c, 'd, 'e) unary_memoized_fun =
-  ( 'a,
-    'b,
+type ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'd, 'int) unary_memoized_fun =
+  ( 'mvbdu_leave_data, 
+    'mvbdu_dic,
     'blist,
     'rlist,
     'vlist,
-    'a ->
+    'mvbdu_leave_data ->
     Exception.exceptions_caught_and_uncaught
     * (Exception.exceptions_caught_and_uncaught ->
       Exception.exceptions_caught_and_uncaught * 'c),
-    'a Mvbdu_sig.mvbdu,
+      'mvbdu_leave_data Mvbdu_sig.mvbdu,
     'd,
-    'e,
-    'a Mvbdu_sig.mvbdu )
+    'int,
+    'mvbdu_leave_data Mvbdu_sig.mvbdu )
   memoized_fun
 
-type ('a,
-       'b,
+type ('mvbdu_leave_data, 
+      'mvbdu_dic,
        'blist,
        'rlist,
        'vlist,
@@ -75,89 +74,89 @@ type ('a,
        'e,
        'f)
      unary_with_threshold_memoized_fun =
-  ( 'a,
-    'b,
+  ( 'mvbdu_leave_data, 
+    'mvbdu_dic,
     'blist,
     'rlist,
     'vlist,
-    'a ->
+    'mvbdu_leave_data ->
     Exception.exceptions_caught_and_uncaught
     * (Exception.exceptions_caught_and_uncaught ->
       Exception.exceptions_caught_and_uncaught * 'c),
-    int * 'a Mvbdu_sig.mvbdu,
+    int * 'mvbdu_leave_data Mvbdu_sig.mvbdu,
     'd,
     'e,
     'f )
   memoized_fun
 
-type ('a, 'b, 'blist, 'rlist, 'vlist, 'c, 'd, 'e) binary_memoized_fun =
-  ( 'a,
-    'b,
+type ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'd, 'int) binary_memoized_fun =
+  ( 'mvbdu_leave_data, 
+    'mvbdu_dic,
     'blist,
     'rlist,
     'vlist,
-    ('a ->
+    ('mvbdu_leave_data ->
     Exception.exceptions_caught_and_uncaught
-    * ('a, 'b, 'blist, 'rlist, 'vlist, 'c, 'd, 'e) unary_memoized_fun)
+    * ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'd, 'int) unary_memoized_fun)
     pair,
-    'a Mvbdu_sig.mvbdu * 'a Mvbdu_sig.mvbdu,
+    'mvbdu_leave_data Mvbdu_sig.mvbdu * 'mvbdu_leave_data Mvbdu_sig.mvbdu,
     'd,
-    'e,
-    'a Mvbdu_sig.mvbdu )
+    'int,
+    'mvbdu_leave_data Mvbdu_sig.mvbdu )
   memoized_fun
 
-type ('a, 'b, 'blist, 'rlist, 'vlist, 'c, 'd, 'e) unary_other_memoized_fun =
-  ( 'a,
-    'b,
+type ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'd, 'e) unary_other_memoized_fun =
+  ( 'mvbdu_leave_data,
+    'mvbdu_dic,
     'blist,
     'rlist,
     'vlist,
-    'a ->
+    'mvbdu_leave_data ->
     Exception.exceptions_caught_and_uncaught
     * (Exception.exceptions_caught_and_uncaught ->
-      Exception.exceptions_caught_and_uncaught * 'a Mvbdu_sig.cell),
-    'd * 'a Mvbdu_sig.mvbdu,
+      Exception.exceptions_caught_and_uncaught * 'mvbdu_leave_data Mvbdu_sig.cell),
+    'd * 'mvbdu_leave_data Mvbdu_sig.mvbdu,
     'c,
     'e,
-    'a Mvbdu_sig.mvbdu )
+    'mvbdu_leave_data Mvbdu_sig.mvbdu )
   memoized_fun
 
-type ('bool, 'b, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'e) reset = {
+type ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'int) reset = {
   empty_range_list:
     Exception.exceptions_caught_and_uncaught
-    * ('bool, 'b, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'e) unary_memoized_fun;
+    * ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'int) unary_memoized_fun;
   empty_association_list:
     Exception.exceptions_caught_and_uncaught
-    * ('bool, 'b, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'e) unary_memoized_fun;
+    * ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'int) unary_memoized_fun;
   empty_variables_list:
     Exception.exceptions_caught_and_uncaught
-    * ('bool, 'b, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'e) unary_memoized_fun;
+    * ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'int) unary_memoized_fun;
   leaf:
-    'bool ->
+    'mvbdu_leave_data ->
     Exception.exceptions_caught_and_uncaught
     * (Exception.exceptions_caught_and_uncaught ->
-      Exception.exceptions_caught_and_uncaught * 'bool Mvbdu_sig.cell);
+      Exception.exceptions_caught_and_uncaught * 'mvbdu_leave_data Mvbdu_sig.cell);
   clean_head:
     Exception.exceptions_caught_and_uncaught
-    * ('bool, 'b, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'e) unary_memoized_fun;
+    * ('mvbdu_leave_data, 'mvbdu_dic, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'int) unary_memoized_fun;
     
   height: Exception.exceptions_caught_and_uncaught
-    * (bool, 'b, 'blist, 'rlist, 'vlist,
+    * (bool, 'mvbdu_dic, 'blist, 'rlist, 'vlist,
     bool ->
     Exception_without_parameter.exceptions_caught_and_uncaught *
     (Exception_without_parameter.exceptions_caught_and_uncaught ->
      Exception_without_parameter.exceptions_caught_and_uncaught *
-     ('bool, bool) Mvbdu_sig.premvbdu),
+     ('mvbdu_leave_data, bool) Mvbdu_sig.premvbdu),
     bool Mvbdu_sig.mvbdu, 'memo_tables, 'c, int)
    memoized_fun;
   
     width: Exception.exceptions_caught_and_uncaught
-    * (bool, 'b, 'blist, 'rlist, 'vlist,
+    * (bool, 'mvbdu_dic, 'blist, 'rlist, 'vlist,
     bool ->
     Exception_without_parameter.exceptions_caught_and_uncaught *
     (Exception_without_parameter.exceptions_caught_and_uncaught ->
      Exception_without_parameter.exceptions_caught_and_uncaught *
-     ('bool, bool) Mvbdu_sig.premvbdu),
+     ('mvbdu_leave_data, bool) Mvbdu_sig.premvbdu),
     bool Mvbdu_sig.mvbdu, 'memo_tables, 'c, int)
    memoized_fun;
   
@@ -166,13 +165,13 @@ type ('bool, 'b, 'blist, 'rlist, 'vlist, 'c, 'memo_tables, 'e) reset = {
     int ->
     Exception.exceptions_caught_and_uncaught
     * (Exception.exceptions_caught_and_uncaught ->
-      Exception.exceptions_caught_and_uncaught * 'bool Mvbdu_sig.cell);
+      Exception.exceptions_caught_and_uncaught * 'mvbdu_leave_data Mvbdu_sig.cell);
   build_true:
     int ->
     int ->
-    'bool Mvbdu_sig.mvbdu ->
-    'bool Mvbdu_sig.mvbdu ->
+    'mvbdu_leave_data Mvbdu_sig.mvbdu ->
+    'mvbdu_leave_data Mvbdu_sig.mvbdu ->
     Exception.exceptions_caught_and_uncaught
     * (Exception.exceptions_caught_and_uncaught ->
-      Exception.exceptions_caught_and_uncaught * 'bool Mvbdu_sig.cell);
+      Exception.exceptions_caught_and_uncaught * 'mvbdu_leave_data Mvbdu_sig.cell);
 }
