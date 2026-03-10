588a5bc3ae7931083446ac59387fae7364baba7c
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2026-03-10 15:44:23 +0100

    Incremental version of scan_compil with logs

--- core/KaSa_rep/frontend/list_tokens.mli
+++ core/KaSa_rep/frontend/list_tokens.mli
@@ -15,6 +15,21 @@ val scan_compil :
   Exception_without_parameter.exceptions_caught_and_uncaught
   * Cckappa_sig.kappa_handler
 
+val scan_incremental_compil : 
+  Remanent_parameters_sig.parameters ->
+  Exception_without_parameter.exceptions_caught_and_uncaught ->
+  ( Ckappa_sig.agent,
+    Ckappa_sig.agent_sig,
+    Ckappa_sig.mixture,
+    Ckappa_sig.mixture,
+    string,
+    Ckappa_sig.mixture Ckappa_sig.rule )
+  Ast.compil ->
+  Cckappa_sig.kappa_handler -> 
+  Exception_without_parameter.exceptions_caught_and_uncaught
+  * Cckappa_sig.kappa_handler 
+
+
 val empty_handler :
   Remanent_parameters_sig.parameters ->
   Exception_without_parameter.exceptions_caught_and_uncaught ->
